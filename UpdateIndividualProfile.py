e38a447f878acfb317653dd0c19a337ee71f3efd
dpraja <raja.pushpanathan@gmail.com>
2018-04-25 12:21:43 +0530

    Update UpdateIndividualProfile.py

--- UpdateIndividualProfile.py
+++ UpdateIndividualProfile.py
@@ -15,57 +15,9 @@ app = Flask(__name__)
 # Below function is called from the root file 
 def UpdateIndividualProfile(request):
     # syntax for get and assign the value from business_id   
-    PF_Firstname = request.json['PF_Firstname']
-    PF_Lastname = request.json['PF_Lastname']
-    PF_Language = request.json['PF_Language']
-    PF_Title = request.json['PF_Title']
-    PF_Mobileno = request.json['PF_Mobileno']
-    PF_Individual_Address = request.json['PF_Individual_Address']
-    PF_Home_Address = request.json['PF_Home_Address']
-    PF_Individual_City = request.json['PF_Individual_City']
-    PF_Individual_Postalcode  = request.json['PF_Individual_Postalcode']
-    PF_Individual_Country = request.json['PF_Individual_Country']
-    PF_Individual_State = request.json['PF_Individual_State']
-    PF_Salutation = request.json['PF_Salutation']
-    PF_Individual_VIP = request.json['PF_Individual_VIP']
-    PF_Nationality = request.json['PF_Nationality']
-    PF_Date_Of_Birth = request.json['PF_Date_Of_Birth']
-    PF_Passport = request.json['PF_Passport']
-    PF_Type = request.json['PF_Type']
-    PF_Individual_Communication1 = request.json['PF_Individual_Communication1']
-    PF_Individual_Communication2 = request.json['PF_Individual_Communication2']
-    PF_Individual_Communication3 = request.json['PF_Individual_Communication3']
-    PF_Individual_Commtype1 = request.json['PF_Individual_Commtype1']
-    PF_Individual_Commtype2 = request.json['PF_Individual_Commtype2']
-    PF_Individual_Commtype3 = request.json['PF_Individual_Commtype3']
+  
 
-    # call gensql for connect DB and execute the query
-    d = {} # define dictionary 
-    d['PF_Firstname'] = PF_Firstname
-    d['PF_Lastname'] = PF_Lastname
-    d['PF_Language'] = PF_Language
-    d['PF_Title'] = PF_Title
-    d['PF_Mobileno'] = PF_Mobileno
-    d['PF_Individual_Address'] = PF_Individual_Address
-    d['PF_Home_Address'] = PF_Home_Address
-    d['PF_Individual_City'] = PF_Individual_City
-    d['PF_Individual_Postalcode'] = PF_Individual_Postalcode
-    d['PF_Individual_Country'] = PF_Individual_Country
-    d['PF_Individual_State'] = PF_Individual_State
-    d['PF_Salutation'] = PF_Salutation
-    d['PF_Individual_VIP'] = PF_Individual_VIP
-    d['PF_Nationality'] = PF_Nationality
-    d['PF_Date_Of_Birth'] = PF_Date_Of_Birth
-    d['PF_Passport'] = PF_Passport
-    d['PF_Type'] = PF_Type
-    d['PF_Individual_Communication1'] = PF_Individual_Communication1
-    d['PF_Individual_Communication2'] = PF_Individual_Communication2
-    d['PF_Individual_Communication3'] = PF_Individual_Communication3
-    d['PF_Individual_Commtype1'] = PF_Individual_Commtype1
-    d['PF_Individual_Commtype2'] = PF_Individual_Commtype2
-    d['PF_Individual_Commtype3'] = PF_Individual_Commtype3
-
-
+    d = request.json
     
     #Include buisness logic before calling Sql Query(Optional)
     sql_value = gensql('insert','profile.pf_individual_profile',d) # eg: insert into test  (business_id)  values ('?') 
@@ -74,6 +26,7 @@ def UpdateIndividualProfile(request):
 
     #Include buisness logic after Sql Query
     data = d.get("PF_Mobileno")
+    data1 = d.get("PF_Firstname")
     print(data)
     PF_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
     PF_Log_Time = PF_Log_Time.time().strftime("%H:%M:%S")
@@ -85,7 +38,7 @@ def UpdateIndividualProfile(request):
     Emp_Lastname = "veroni"
     PF_Action_Type = "New Profile"
     PF_Mobileno = data
-    PF_Log_Description = "Create Individual Profile" + " "+PF_Firstname
+    PF_Log_Description = "Create Individual Profile" + " "+data1
     s = {}
     s['Emp_Id'] = Emp_Id
     s['Emp_Firstname'] = Emp_Firstname
