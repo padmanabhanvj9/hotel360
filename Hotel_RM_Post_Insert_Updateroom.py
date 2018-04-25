80a26e2d5eb88f603b0b509e39ad2d3fdb661a02
padmanabhanvj9 <padmanabhanvj9@gmail.com>
2018-04-25 18:34:37 +0530

    Update Hotel_RM_Post_Insert_Updateroom.py

--- Hotel_RM_Post_Insert_Updateroom.py
+++ Hotel_RM_Post_Insert_Updateroom.py
@@ -4,20 +4,24 @@ import json
 def hotel_rm_post_insert_updateroomlist(request):
     d = request.json
     print(gensql('insert','room_management.RM_Room_List',d))
-    return("success")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+
 def hotel_rm_post_insert_updateroomcondition(request):
     d = request.json
     print(gensql('insert','room_management.RM_Room_Condition',d))
-    return("success")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+
 def hotel_rm_post_insert_updateoutoforderservice(request):
     d = request.json
     print(gensql('insert','room_management.RM_Room_Mainteanance',d))
-    return("success")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+
 def hotel_rm_post_insert_updateroommaintenance(request):
     d = request.json
     d['rm_resolvedon'] = 'unresolved' 
     print(gensql('insert','room_management.RM_Room_Mainteanance_Acitivity_Log',d))
-    return("success")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+
 
 
 
