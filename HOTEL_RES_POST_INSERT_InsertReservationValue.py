38a714e19e0ea56031e2e06d7dd71ea47aee4f8e
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-05-22 15:01:34 +0530

    Add files via upload

---
+++ HOTEL_RES_POST_INSERT_InsertReservationValue.py
@@ -0,0 +1,32 @@
+from sqlwrapper import gensql
+import json
+def Hotel_RES_POST_INSERT_RestypeInsert(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.restype',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+
+def Hotel_RES_POST_INSERT_Alertarea(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.alertarea',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+
+def Hotel_RES_POST_INSERT_Alertcode(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.alertarea',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+
+def Hotel_RES_POST_INSERT_Origin(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.origin',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+
+def Hotel_RES_POST_INSERT_Source(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.res_source',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+
