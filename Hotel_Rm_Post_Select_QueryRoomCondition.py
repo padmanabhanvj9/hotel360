6fc37450e23968cb5645711e5d6c1c5d724815f5
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-05-28 12:04:26 +0530

    Update Hotel_Rm_Post_Select_QueryRoomCondition.py

--- Hotel_Rm_Post_Select_QueryRoomCondition.py
+++ Hotel_Rm_Post_Select_QueryRoomCondition.py
@@ -2,7 +2,7 @@ from sqlwrapper import dbget
 import json
 
 def hotel_rm_post_select_queryroomcondition(request):
-    sql = 'select rm_room,rm_room_type,rm_room_status,rm_fo_status,rm_features,rm_room_condition from room_management.rm_room_list order by rm_room'
+    sql = 'select rm_room,rm_room_type,rm_room_status,rm_fo_status,rm_features,rm_room_condition,rm_reservation_status from room_management.rm_room_list order by rm_room'
     print(sql)      
     db_res = (dbget(sql))
     print(db_res)
