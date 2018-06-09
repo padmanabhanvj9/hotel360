04cc248e06428ef4644fe9e8519b281925b18341
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-09 16:29:04 +0530

    Update Hotel_RES_Post_Insert_UpdateGuestPrivileges.py

--- Hotel_RES_Post_Insert_UpdateGuestPrivileges.py
+++ Hotel_RES_Post_Insert_UpdateGuestPrivileges.py
@@ -7,12 +7,12 @@ def Hotel_RES_Post_Insert_UpdateGuestPrivileges(request):
 def Hotel_RES_Post_Update_UpdateGuestPrivileges(request):
     d,e = {},{}
     d['privileges_key'] = request.json['privileges_key']
-    e['pf_mobileno'] = request.json['pf_mobileno']
+    e['res_id'] = request.json['res_id']
     sql_value = gensql('update','reservation.res_guest_privileges',d,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 def Hotel_RES_Get_Select_QueryGuestPrivileges(request):
     e = {}
-    e['pf_mobileno'] = request.args['pf_mobileno']
+    e['res_id'] = request.args['res_id']
     sql_value = gensql('select','reservation.res_guest_privileges','privileges_key',e)
     sql_value = json.loads(sql_value)
     print(sql_value)
