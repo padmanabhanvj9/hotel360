533ca6a14acac59715c462f489a87a811de4ec38
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-09 12:24:37 +0530

    Update HOTEL_RES_GET_SELECT_QueryAlertReservation.py

--- HOTEL_RES_GET_SELECT_QueryAlertReservation.py
+++ HOTEL_RES_GET_SELECT_QueryAlertReservation.py
@@ -1,15 +1,15 @@
 from sqlwrapper import gensql
 from flask import Flask,request, jsonify
 import json
-app = Flask(__name__)
+
 def HOTEL_RES_GET_SELECT_QueryAlertReservation():
     
-    PF_Mobileno = request.args['PF_Mobileno']
-    RES_Id = request.args['RES_Id']
+    
+    Res_id = request.args['Res_id']
     d = {}
   
-    d['PF_Mobileno'] = PF_Mobileno
-    d['RES_Id'] = RES_Id
+  
+    d['Res_id'] = Res_id
     sql_value = gensql('select','reservation.res_alert','*',d)
     sql_value = json.loads(sql_value)
     print(sql_value)
