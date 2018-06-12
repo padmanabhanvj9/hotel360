f1173e6085041bcf13bfa101c0a91a7b993cfd5c
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 17:24:04 +0530

    Update UpdateProfileNotesRecord.py

--- UpdateProfileNotesRecord.py
+++ UpdateProfileNotesRecord.py
@@ -1,17 +1,10 @@
 from sqlwrapper import gensql
-from flask import Flask,request, jsonify
 import json
-app = Flask(__name__)
+
 def UpdateProfileNotesRecord(request):
-    d,e = {},{}
-    res = request.json
-    print(res)
-    for name,val in res.items():
- 
-       if  name == 'PF_Mobileno' or name == 'PF_Firstname':
-          e[name] = ""+val+""
-       else :
-           d[name] = ""+val+""
+    d1 = request.json
+    e = { k : v for k,v in d1.items() if k in ('pf_id')}
+    d = { k : v for k,v in d1.items() if k not in ('pf_id')}
     print(e,d)
     sql_value = gensql('update','profile.pf_notes',d,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
