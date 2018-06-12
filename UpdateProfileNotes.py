11b5e73e7cc1969cb57971df95f646b68d47b439
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 17:17:00 +0530

    Update UpdateProfileNotes.py

--- UpdateProfileNotes.py
+++ UpdateProfileNotes.py
@@ -3,4 +3,5 @@ import json
 
 def UpdateProfileNotes(request):
     d = request.json
+    sql_value = gensql('insert','profile.pf_notes',d)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
