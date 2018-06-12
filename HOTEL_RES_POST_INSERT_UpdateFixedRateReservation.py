bfc390331009066b74e28429ff1c36373e8f08c5
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 15:00:02 +0530

    Update HOTEL_RES_POST_INSERT_UpdateFixedRateReservation.py

--- HOTEL_RES_POST_INSERT_UpdateFixedRateReservation.py
+++ HOTEL_RES_POST_INSERT_UpdateFixedRateReservation.py
@@ -4,5 +4,10 @@ import json
 
 def HOTEL_RES_POST_INSERT_UpdateFixedRateReservation(request):
     d = request.json
-    sql_value = gensql('insert','reservation.res_fixed_rate',d)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
+    print(d)
+    a = { k : v for k,v in d.items() if v != ''}
+    print(a)
+    sql_value = gensql('insert','reservation.res_fixed_rate',a)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+	
