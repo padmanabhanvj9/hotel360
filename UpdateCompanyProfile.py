69dfcd123e7fde2fea76984fbe4b64af3730e241
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 15:04:50 +0530

    Update UpdateCompanyProfile.py

--- UpdateCompanyProfile.py
+++ UpdateCompanyProfile.py
@@ -1,35 +1,42 @@
-from sqlwrapper import gensql
+#Input Param: 
+#OutputParam: Record Inserted Successfully
+#Purpose: This service used for crate company profile
+#Date:13/03/2018
+#UpdateDate:11/06/2018
+#Author: Aravinth
+
+from sqlwrapper import gensql,dbget
 import datetime
-from flask import Flask,request, jsonify
 import json
-app = Flask(__name__)
 def UpdateCompanyProfile(request):
     d = request.json
-    sql_value = gensql('insert','profile.pf_company_profile',d)
-    data = d.get("PF_Mobileno")
+    select = json.loads(dbget("select count(*) from profile.pf_company_profile"))
+    print(select[0]['count'])
+    id1 = "cpy"+str(select[0]['count']+1)
+    print(id1)
+    d['pf_id'] = id1
+    sql_value = gensql('insert','profile.pf_company_profile',d) 
+
     data1 = d.get("PF_Firstname")
-    print(data)
+    pf_type = d['PF_Type']
+    
     PF_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
     PF_Log_Time = PF_Log_Time.time().strftime("%H:%M:%S")
     print(PF_Log_Time)
     PF_Log_Date = datetime.datetime.utcnow().date()
     print(PF_Log_Date)
-    Emp_Id = '121'
-    Emp_Firstname = "daisy"
-    Emp_Lastname = "veroni"
-    PF_Action_Type = "New Profile"
-    PF_Mobileno = data
-    PF_Log_Description = "Create Company Profile" + " "+data1
+    
+    PF_Log_Description = "Create "+pf_type+" Profile" + " "+data1
     s = {}
-    s['Emp_Id'] = Emp_Id
-    s['Emp_Firstname'] = Emp_Firstname
-    s['Emp_Lastname'] = Emp_Lastname
+    s['Emp_Id'] = '121'
+    s['Emp_Firstname'] = "daisy"
+    s['Emp_Lastname'] = "veroni"
     s['PF_Log_Date'] = PF_Log_Date
     s['PF_Log_Time'] = PF_Log_Time
-    s['PF_Action_Type'] = PF_Action_Type
+    s['PF_Action_Type'] = "New Profile"
     s['PF_Log_Description'] = PF_Log_Description
-    s['PF_Mobileno'] = PF_Mobileno
-    
+    s['pf_id'] = id1
     
     sql_value = gensql('insert','profile.pf_profile_activitylog',s)
+    # finally return the value from DB_Wrapper   
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
