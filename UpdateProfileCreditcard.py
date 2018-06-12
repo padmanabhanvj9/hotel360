c1a3e1cbd26f1a15a029a62a7a6c058268539c4e
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 14:58:59 +0530

    Update UpdateProfileCreditcard.py

--- UpdateProfileCreditcard.py
+++ UpdateProfileCreditcard.py
@@ -1,22 +1,8 @@
 from sqlwrapper import gensql
-from flask import Flask,request, jsonify
 import json
-app = Flask(__name__)
-def UpdateProfileCreditcard(request):
-    PF_Firstname = request.json['PF_Firstname']
-    PF_Mobileno = request.json['PF_Mobileno']
-    PF_Sequence = request.json['PF_Sequence']
-    PF_Creditcard_No = request.json['PF_Creditcard_No']
-    PF_Card_Type = request.json['PF_Card_Type']
-    PF_Expiration_Date = request.json['PF_Expiration_Date']
 
-    d = {}
-    d['PF_Firstname'] = PF_Firstname
-    d['PF_Mobileno'] = PF_Mobileno
-    d['PF_Sequence'] = PF_Sequence
-    d['PF_Creditcard_No'] = PF_Creditcard_No
-    d['PF_Card_Type']  = PF_Card_Type
-    d['PF_Expiration_Date'] = PF_Expiration_Date
+def UpdateProfileCreditcard(request):
+    d = request.json
     sql_value = gensql('insert','profile.pf_creditcard',d)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
