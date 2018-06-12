b3b0191639d5d31c82905ccda8997e37c763d469
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-13 00:41:10 +0530

    Update UpdateProfilePreference.py

--- UpdateProfilePreference.py
+++ UpdateProfilePreference.py
@@ -3,10 +3,10 @@ import json
 
 def UpdateProfilePreference(request):
     d = request.json
-    print(d)
-    e = { k : v for k,v in d.items() if k in ('pf_id')}
-    print(e)
-    f = { k : v for k,v in d.items() if k not in ('pf_id') if v !='' }
+    #print(d)
+    e = { k : v for k,v in d.items() if k in ('pf_id,preference_id')}
+    #print(e)
+    f = { k : v for k,v in d.items() if k not in ('pf_id,preference_id') }
     print(f)    
     sql_value = gensql('update','profile.pf_preference',f,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
