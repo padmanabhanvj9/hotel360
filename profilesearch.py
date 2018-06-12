6459bf8063d940d94b2796e4f10a8247e49f7ae3
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-12 18:06:02 +0530

    Update profilesearch.py

--- profilesearch.py
+++ profilesearch.py
@@ -7,12 +7,12 @@ def HOTEL_PF_GET_SELECT_QueryProfileSearch():
   
     sql_value = gensql('select','profile.pf_company_profile','*')
     sql_value1 = json.loads(sql_value)
-    print(sql_value1)
+    #print(sql_value1)
 
     
     sql_value = gensql('select','profile.pf_individual_profile','*')
     sql_value2 = json.loads(sql_value)
-    print(sql_value2)
+    #print(sql_value2)
     result = []
     result = sql_value1 + sql_value2
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':result  ,'ReturnCode':'RRTS'},indent=4))
