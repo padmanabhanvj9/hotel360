67f47a6cd31dcfeb8d6ef718dc2a7d4ea7c34946
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-13 12:47:23 +0530

    Update Hotel_RES_Post_Insert_UpdateGuestTraces.py

--- Hotel_RES_Post_Insert_UpdateGuestTraces.py
+++ Hotel_RES_Post_Insert_UpdateGuestTraces.py
@@ -8,9 +8,9 @@ def Hotel_RES_Post_Insert_UpdateGuestTraces(request):
 def Hotel_RES_Post_Update_UpdateGuestTraces(request):
     d = request.json
     print(d)
-    a = { k : v for k,v in d.items() if v != '' if k not in ('res_id','fixed_charges_id')}
+    a = { k : v for k,v in d.items() if v != '' if k not in ('res_id','traces_id')}
     print(a)
-    e = { k : v for k,v in d.items() if k != '' if k in ('res_id','fixed_charges_id')}
+    e = { k : v for k,v in d.items() if k != '' if k in ('res_id','traces_id')}
     print(e)
     sql_value = gensql('update','reservation.res_traces',a,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
