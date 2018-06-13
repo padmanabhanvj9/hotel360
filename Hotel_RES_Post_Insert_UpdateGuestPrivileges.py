6a52e80e7c454185193d2912102fa24cdde8788f
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-13 12:27:31 +0530

    Update Hotel_RES_Post_Insert_UpdateGuestPrivileges.py

--- Hotel_RES_Post_Insert_UpdateGuestPrivileges.py
+++ Hotel_RES_Post_Insert_UpdateGuestPrivileges.py
@@ -5,15 +5,18 @@ def Hotel_RES_Post_Insert_UpdateGuestPrivileges(request):
     sql_value = gensql('insert','reservation.res_guest_privileges',d)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 def Hotel_RES_Post_Update_UpdateGuestPrivileges(request):
-    d,e = {},{}
-    d['privileges_key'] = request.json['privileges_key']
-    e['res_id'] = request.json['res_id']
+    d = request.json
+    a = { k : v for k,v in d.items() if v != '' if k not in ('Res_id','privileges_id')}
+    print(a)
+    e = { k : v for k,v in d.items() if k != '' if k in ('Res_id','privileges_id')}
+
+    print(e)
     sql_value = gensql('update','reservation.res_guest_privileges',d,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 def Hotel_RES_Get_Select_QueryGuestPrivileges(request):
     e = {}
-    e['res_id'] = request.args['res_id']
-    sql_value = gensql('select','reservation.res_guest_privileges','privileges_key',e)
+    e['res_id'] = request.json['res_id']
+    sql_value = gensql('select','reservation.res_guest_privileges','*',e)
     sql_value = json.loads(sql_value)
     print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value  ,'ReturnCode':'RRTS'},indent=4))
