b7f1a1476590ceee713cd19b04b8db38a32b26e0
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-13 11:55:55 +0530

    Update HOTEL_RES_POST_UPDATE_UpdateFixedRateReservation.py

--- HOTEL_RES_POST_UPDATE_UpdateFixedRateReservation.py
+++ HOTEL_RES_POST_UPDATE_UpdateFixedRateReservation.py
@@ -6,9 +6,9 @@ def HOTEL_RES_POST_UPDATE_UpdateFixedRateReservation(request):
  
     d = request.json
     print(d)
-    a = { k : v for k,v in d.items() if v != '' if k not in ('Res_id')}
+    a = { k : v for k,v in d.items() if v != '' if k not in ('Res_id','Fixed_rate_id')}
     print(a)
-    e = { k : v for k,v in d.items() if k != '' if k in ('Res_id')}
+    e = { k : v for k,v in d.items() if k != '' if k in ('Res_id','Fixed_rate_id')}
 
     print(e)
     sql_value = gensql('update','reservation.res_fixed_rate',a,e)
