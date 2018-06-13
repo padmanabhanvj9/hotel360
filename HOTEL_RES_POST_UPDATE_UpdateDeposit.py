63d05a27a9abcf481598dc223b3148d7b51cc89c
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-13 13:17:51 +0530

    Update HOTEL_RES_POST_UPDATE_UpdateDeposit.py

--- HOTEL_RES_POST_UPDATE_UpdateDeposit.py
+++ HOTEL_RES_POST_UPDATE_UpdateDeposit.py
@@ -1,16 +1,13 @@
 from sqlwrapper import gensql
 import json
 def HOTEL_RES_POST_UPDATE_UpdateDeposit(request):
-    
-    d,e = {},{}
-    res = request.json
-    print(res)
-    for name,val in res.items():
- 
-       if  name == 'PF_Mobileno' or name == 'PF_Firstname':
-          e[name] = ""+val+""
-       else :
-           d[name] = ""+val+""
-    print(e,d)
+
+    d = request.json
+    print(d)
+    a = { k : v for k,v in d.items() if v != '' if k not in ('Res_id','deposit_id')}
+    print(a)
+    e = { k : v for k,v in d.items() if k != '' if k in ('Res_id','deposit_id')}
+
+    print(e)
     sql_value = gensql('update','reservation.res_deposit',d,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
