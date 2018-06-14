31b18771310447cbdedbbdc292528c3232dc3cfb
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-14 16:37:49 +0530

    Update HOTEL_RES_GET_SELECT_QueryWaitlistReservation.py

--- HOTEL_RES_GET_SELECT_QueryWaitlistReservation.py
+++ HOTEL_RES_GET_SELECT_QueryWaitlistReservation.py
@@ -1,10 +1,8 @@
 from sqlwrapper import gensql
 import json
-from flask import Flask,request,jsonify
-
-def HOTEL_RES_GET_SELECT_QueryWaitlistReservation():
-   
-    sql_value = gensql('select','reservation.res_waitlist','*')
+def HOTEL_RES_GET_SELECT_QueryWaitlistReservation(request):
+    d = request.json
+    sql_value = gensql('select','reservation.res_waitlist','*',d)
     sql_value1 = json.loads(sql_value)
     print(sql_value1)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value1  ,'ReturnCode':'RRTS'},indent=4))
