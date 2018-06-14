491a2d99a38497481b8df492f266afe0302959c3
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-14 12:05:01 +0530

    Update HOTEL_RES_POST_INSERT_UpdateNewReservation.py

--- HOTEL_RES_POST_INSERT_UpdateNewReservation.py
+++ HOTEL_RES_POST_INSERT_UpdateNewReservation.py
@@ -1,11 +1,26 @@
 import datetime
-from sqlwrapper import gensql
+from sqlwrapper import gensql, dbget, dbput
 import random
 import json
 
 
 def HOTEL_RES_POST_INSERT_UpdateNewReservation(request):
     d = request.json
+    PF_Mobileno = str(d.get("PF_Mobileno"))
+    print(PF_Mobileno)
+    RES_Arrival = str(d.get("RES_Arrival"))
+    print(RES_Arrival)
+    RES_Depature = str(d.get("RES_Depature"))
+    sqlcount = ("select count(*) from reservation.res_reservation \
+                 where pf_mobileno = '"+PF_Mobileno+"' and RES_Arrival = '"+RES_Arrival+"' and RES_Depature = '"+RES_Depature+"'")
+    countdata = dbget(sqlcount)
+    countdata = json.loads(countdata)
+    print(countdata)
+    print(countdata[0]['count'],type(countdata[0]['count']))
+
+    if countdata[0]['count'] > 0:
+        return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Reservation Already Exist','ReturnCode':'RAE'}, sort_keys=True, indent=4)) 
+
     RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
     RES_Log_Time = RES_Log_Time.time().strftime("%H:%M:%S")
     print(RES_Log_Time)
@@ -15,8 +30,8 @@ def HOTEL_RES_POST_INSERT_UpdateNewReservation(request):
     Emp_Firstname = "Ranimangama"
     d['created_on'] = RES_Log_Date
     d['created_by'] = Emp_Firstname
+    
     random_no = (random.randint(1000000000,9999999999))
-   
     random_no = str(random_no)
     random_no = random_no[0:4]
     print(random_no)
@@ -27,13 +42,27 @@ def HOTEL_RES_POST_INSERT_UpdateNewReservation(request):
     print(mobile)
     RES_Confnumber = "PMS" + conf
     print(RES_Confnumber)
-    #RES_Confnumber = int(RES_Confnumber)
     d['RES_Confnumber'] = RES_Confnumber
-    
+    d['RES_Guest_Status'] = "Reserved"
+    select = json.loads(dbget("select * from reservation.res_id"))
+    print(select,type(select),len(select))
+    print(select[0]['id'])
+    Res_id = (select[0]['id']+1)
+    print(Res_id)
+    update = dbput("update reservation.res_id set id = '"+str(select[0]['id']+1)+"'")
+    d['Res_id'] = Res_id
+  
     sql_value = gensql('insert','reservation.res_reservation',d)
-    #print(d)
+    print(d)
     data = d.get("PF_Firstname")
     number = d.get("RES_Confnumber")
+    t = {}
+    t['RES_Confnumber'] = number
+    reservation_id = gensql('select','reservation.res_reservation','res_id',t)
+    reservation_id = json.loads(reservation_id)
+    print(reservation_id[0]['res_id'],type(reservation_id[0]['res_id']))
+    reservation_id = reservation_id[0]['res_id']
+
     print(data)
     
     
@@ -47,6 +76,7 @@ def HOTEL_RES_POST_INSERT_UpdateNewReservation(request):
     s['RES_Log_Time'] = RES_Log_Time
     s['RES_Action_Type'] = RES_Action_Type
     s['RES_Description'] = RES_Description
+    s['Res_id'] = reservation_id
     
     sql_value = gensql('insert','reservation.res_activity_log',s)
 
