f66a4e94c52ec71060f7796c35077290f25ffa1b
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-19 16:37:39 +0530

    Update HOTEL_FD_POST_INSERT_UpdateQueueRreservation.py

--- HOTEL_FD_POST_INSERT_UpdateQueueRreservation.py
+++ HOTEL_FD_POST_INSERT_UpdateQueueRreservation.py
@@ -1,54 +1,54 @@
-import psycopg2
 import datetime
-from flask import Flask,request, jsonify
+from sqlwrapper import gensql,dbget,dbput
 import json
 def HOTEL_FD_POST_INSERT_UpdateQueueRreservation(request):
-    RM_Qtime = request.json['RM_Qtime']
-    RM_Room = request.json['RM_Room']
-    RM_Room_Type = request.json['RM_Room_Type']
-    RM_Room_Status = request.json['RM_Room_Status']
-    RM_FO_Status = request.json['RM_FO_Status']
-    PF_Firstname = request.json['PF_Firstname']
-    RM_VIP = request.json['RM_VIP']
-    PF_Mobileno = request.json['PF_Mobileno']
-    RM_Room_Class = request.json['RM_Room_Class']
     
-
-    #RM_Queue = request.json['RM_Queue']
+    d = request.json
+    res_id = d.get("Res_id")
+    rm_room = d.get("rm_room")
+    print(rm_room,type(rm_room))
+    rm_room  = rm_room.split(',')
+    print(rm_room,type(rm_room))
+    rm_room = str(rm_room)[1:-1]
+    print(rm_room)
+    RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
+    RES_Log_Time = RES_Log_Time.time().strftime("%H:%M:%S")
+    print(RES_Log_Time)
     RM_Queue_Date = datetime.datetime.utcnow().date()
     print(RM_Queue_Date)
     RM_Queue_Date = str(RM_Queue_Date)
-    con = psycopg2.connect(user='akvwghpxnyalss',password='bc0080c7c0f0b55e162582666cf6b39227c6160e2759dbb0d7f28d58190952cd',host='ec2-54-235-146-184.compute-1.amazonaws.com',port='5432',database='dfbvc8j8egd076')
-    cur = con.cursor()
-    #a = { k : v for k,v in d.items() if k == 'PF_Mobileno'}
-    #sql_value = gensql('select','room_management.rm_queue_room','count(*)',a)
-    sql = "select count(*)from room_management.rm_queue_room where  rm_queue_date='"+RM_Queue_Date+"'"
+    
+    psql = dbget("select rm_room_type,rm_room_status,rm_fo_status,rm_room_class from room_management.rm_room_list \
+                          where rm_room in ("+rm_room+")")
+    print(psql,type(psql))
+    psql = json.loads(psql)
+    print(psql)
+    room_type = psql[0]['rm_room_type']
+    print(room_type)
+    room_status = psql[0]['rm_room_status']
+    fo_status = psql[0]['rm_fo_status']
+    room_class = psql[0]['rm_room_class']
+    
+    sql = dbget("select count(*)from room_management.rm_queue_room")
+    sql = json.loads(sql)
+    i = str(sql[0]['count'])
+    print(sql[0]['count'])
+    print(i,type(i))
+    
+    for number in i: 
+         print(number,type(number))
+         number = int(number)
+         number += 1
+    print(number)
+    s={}
+    s['rm_queue'] = number
+    s['res_id'] = res_id
+    s['rm_qtime'] = RES_Log_Time
+    s['rm_room_type'] = room_type
+    s['rm_room_class'] = room_class
+    s['rm_fo_status'] = fo_status
+    s['rm_room_status'] = room_status
+    sql = gensql('insert','room_management.rm_queue_room',s)
     print(sql)
-    cur.execute(sql)
-    i = cur.fetchone()
-    for queue_count in i :
-        queue_count = int(queue_count)
-        print(queue_count)
-    if queue_count is 0 :
-             sql = "insert into  room_management.rm_queue_room VALUES ('"+RM_Qtime+"','"+RM_Room+"','"+RM_Room_Type+"','"+RM_Room_Status+"','"+RM_FO_Status+"','"+PF_Firstname+"','"+RM_VIP+"',"+PF_Mobileno+",'"+RM_Room_Class+"','0','"+RM_Queue_Date+"')"
-             print(sql)
-             cur.execute(sql)
-             con.commit()
 
-    #sql_value = gensql('insert','room_management.rm_queue_room',d)   
-    #return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
-
-    sql= "select rm_queue from room_management.rm_queue_room where rm_queue_date='"+RM_Queue_Date+"'"
-    cur.execute(sql)
-    i = cur.fetchone()
-    for no in i: 
-         print(no,type(no))
-         no = int(no)
-    no+=1
-    print(no,type(no))
-    no = str(no)
-    sql = ("update room_management.rm_queue_room set rm_queue='"+no+"' where rm_queue_date= '"+RM_Queue_Date+"' and pf_mobileno = "+PF_Mobileno+"")
-    print(sql)
-    cur.execute(sql)
-    con.commit()
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
