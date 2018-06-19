e6b6fe323cba38de8f9124ec58a55fa12b457ff8
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-19 19:58:06 +0530

    Update Hotel_RM_Post_Update_Updateroomstatus.py

--- Hotel_RM_Post_Update_Updateroomstatus.py
+++ Hotel_RM_Post_Update_Updateroomstatus.py
@@ -1,27 +1,27 @@
 from sqlwrapper import dbput,gensql
 import json
 
-def hotel_rm_post_update_updateroomstatus(request):
-    if request.args.get('RM_Room_Status') and request.args.get('RM_Room'):
-        room_status = request.args['RM_Room_Status']
-        room_no = request.args['RM_Room']
+def Hotel_Rm_Post_Update_Updateroomstatus(request):
+    if request.json.get('RM_Room_Status') and request.json.get('RM_Room'):
+        room_status = request.json['RM_Room_Status']
+        room_no = request.json['RM_Room']
         d,e={},{}
         d['RM_Room_Status'] = room_status
         e['RM_Room'] = room_no
         gensql('update','room_management.RM_Room_List',d,e)
         return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
-    elif  request.args.get('RM_Room_Status') and request.args.get('Room_List'):
-        room_status = request.args['RM_Room_Status']
-        room_list = request.args['Room_List']
+    elif  request.json.get('RM_Room_Status') and request.json.get('Room_List'):
+        room_status = request.json['RM_Room_Status']
+        room_list = request.json['Room_List']
         a= dbput("update room_management.RM_Room_List  set  RM_Room_Status='"+room_status+"'  where  RM_Room in ("+room_list+")")
         print(a)
         return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
-    elif  request.args.get('RM_Room_Status') and request.args.get('From_Room') and request.args.get('To_Room'):
-        room_status = request.args['RM_Room_Status']
-        from_room = request.args['From_Room']
-        to_room = request.args['To_Room']
+    elif  request.json.get('RM_Room_Status') and request.json.get('From_Room') and request.json.get('To_Room'):
+        room_status = request.json['RM_Room_Status']
+        from_room = request.json['From_Room']
+        to_room = request.json['To_Room']
         from_room = int(from_room)
         to_room = int(to_room)  
         stri = ''
