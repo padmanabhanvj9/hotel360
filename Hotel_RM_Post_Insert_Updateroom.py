16995e959d8ab67f3e8a8d2009e2bdd0b4aadcd6
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-19 19:56:40 +0530

    Update Hotel_RM_Post_Insert_Updateroom.py

--- Hotel_RM_Post_Insert_Updateroom.py
+++ Hotel_RM_Post_Insert_Updateroom.py
@@ -1,5 +1,6 @@
-from sqlwrapper import gensql
+from sqlwrapper import gensql,dbget
 import json
+import datetime
 
 def hotel_rm_post_insert_updateroomlist(request):
     d = request.json
@@ -12,16 +13,56 @@ def hotel_rm_post_insert_updateroomcondition(request):
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 def hotel_rm_post_insert_updateoutoforderservice(request):
-    d = request.json
-    print(gensql('insert','room_management.RM_Room_Mainteanance',d))
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+    if request.json.get('From_Room') and request.json.get('To_Room'):
+        d = request.json
+        print(d)
+        e = { k : v for k,v in d.items() if k not in ('From_Room','To_Room')}
+        from_room = request.json['From_Room']
+        to_room = request.json['To_Room']
+        from_room = int(from_room)
+        to_room = int(to_room)  
+        stri = []
+        for i in range(from_room,to_room+1):
+            stri.append(i)
+        print(stri)
+        for j in stri:
+            e['RM_Room'] = j
+            print(gensql('insert','room_management.rm_room_oo',e))
+        return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))    
+    else:
+        d = request.json
+        print(gensql('insert','room_management.rm_room_oo',d))
+        return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 def hotel_rm_post_insert_updateroommaintenance(request):
     d = request.json
-    d['rm_resolvedon'] = 'unresolved' 
-    print(gensql('insert','room_management.RM_Room_Mainteanance_Acitivity_Log',d))
+    room = request.json['rm_room']
+    d['mainteanance_user'] = 'SUPERVISOR'
+    print(d)
+    e = { k : v for k,v in d.items() if k  in ('rm_room')}
+    f = { k : v for k,v in d.items() if k not  in ('rm_room')}
+    #d['rm_resolvedon'] = 'unresolved'
+    sql = json.loads(dbget("select * from room_management.rm_room_mainteanance_acitivity_log where rm_room = '"+room+"' "))
+    print(sql,type(sql),len(sql))
+    if len(sql) != 0 :
+        d['rm_last_change'] = sql[0]['rm_resolvedon']
+        d['rm_resolvedon'] =  ''
+        d['rm_resolvedby'] = ''        
+        print(gensql('update','room_management.rm_room_mainteanance_acitivity_log',d,e))
+    else:    
+        print(gensql('insert','room_management.RM_Room_Mainteanance_Acitivity_Log',d))
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
+def hotel_rm_post_update_updateroommaintenance(request):
+    e = request.json
+    print(e)
+    d = {}
+    d['rm_resolvedon'] =  datetime.datetime.utcnow().date()
+    d['rm_resolvedby'] = 'SUPERVISOR'
+    sql = json.loads(dbget("select * from room_management.rm_room_mainteanance_acitivity_log where rm_room = '"+e['rm_room']+"' "))
+    print(sql,type(sql))
+    print(gensql('update','room_management.rm_room_mainteanance_acitivity_log',d,e))
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
 
 
