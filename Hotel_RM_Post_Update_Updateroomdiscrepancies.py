b714b108ec83777199263a1e6a65fc96374101a1
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-19 20:07:02 +0530

    Update Hotel_RM_Post_Update_Updateroomdiscrepancies.py

--- Hotel_RM_Post_Update_Updateroomdiscrepancies.py
+++ Hotel_RM_Post_Update_Updateroomdiscrepancies.py
@@ -2,6 +2,7 @@ from sqlwrapper import gensql,dbfetch
 import json
 
 def hotel_rm_post_update_updateroomdiscrepancies(request):
+    i = {}
     if request.json.get('RM_Room') and request.json.get('RM_HK_Status'):
         RM_Room = request.json['RM_Room']
         RM_HK_Status = request.json['RM_HK_Status']
@@ -9,15 +10,18 @@ def hotel_rm_post_update_updateroomdiscrepancies(request):
         res = res[0]
         d,e = {},{}
         e['RM_Room'] = RM_Room
-        d['RM_HK_Status'] = RM_HK_Status        
+        d['RM_HK_Status'] = RM_HK_Status
+        print(gensql('update','room_management.RM_Room_List',d,e))
         if res != RM_HK_Status:
             print(res,RM_HK_Status)
-            if res == 'occupied' and RM_HK_Status == 'vacant':
-               d['rm_room_discrepancy'] = 'sleep'
+            if res == 'occupied' and RM_HK_Status == 'vaccant':
+               i['rm_room_discrepancy'] = 'sleep'
+               i['rm_room'] = RM_Room
             else:
-               d['rm_room_discrepancy'] = 'skip' 
-            print(gensql('update','room_management.RM_Room_List',d,e))
-        print(gensql('update','room_management.RM_Room_List',d,e))    
+               i['rm_room_discrepancy'] = 'skip'
+               i['rm_room'] = RM_Room
+            print(gensql('insert','room_management.rm_room_discrepancy',i))
+        #print(gensql('update','room_management.RM_Room_List',d,e))    
     elif request.json.get('RM_Room') and request.json.get('RM_HK_Person'):
         RM_Room = request.json['RM_Room']
         RM_HK_Person = request.json['RM_HK_Person']
@@ -27,10 +31,11 @@ def hotel_rm_post_update_updateroomdiscrepancies(request):
         d,e = {},{}
         e['RM_Room'] = RM_Room
         d['RM_HK_Person'] = RM_HK_Person
+        print(gensql('update','room_management.RM_Room_List',d,e))
         if res != RM_HK_Person:    
-            d['rm_room_discrepancy'] = 'person'
-            print(gensql('update','room_management.RM_Room_List',d,e))
-        print(gensql('update','room_management.RM_Room_List',d,e))    
+            i['rm_room_discrepancy'] = 'person'
+            i['rm_room'] = RM_Room
+            print(gensql('insert','room_management.rm_room_discrepancy',i))    
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
     
