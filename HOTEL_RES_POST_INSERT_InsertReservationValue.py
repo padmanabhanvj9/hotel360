dd71e5624a8ed4ed78cf09adf8a9cbee968faeb9
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 12:33:42 +0530

    Update HOTEL_RES_POST_INSERT_InsertReservationValue.py

--- HOTEL_RES_POST_INSERT_InsertReservationValue.py
+++ HOTEL_RES_POST_INSERT_InsertReservationValue.py
@@ -65,4 +65,9 @@ def Hotel_RES_POST_INSERT_CancelReason(request):
     sql_value = gensql('insert','reservation.cancel_reason',d)
     print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
+def Hotel_RES_POST_INSERT_Cardtype(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.cardtype',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
 
