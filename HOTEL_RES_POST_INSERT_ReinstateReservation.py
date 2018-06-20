b716c7222932026b48f07ea4384ac954cc5d9e7b
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 11:28:35 +0530

    Update HOTEL_RES_POST_INSERT_ReinstateReservation.py

--- HOTEL_RES_POST_INSERT_ReinstateReservation.py
+++ HOTEL_RES_POST_INSERT_ReinstateReservation.py
@@ -8,10 +8,10 @@ def HOTEL_RES_POST_INSERT_ReinstateReservation(request):
     
     res_id = d.get("res_id")
     sql_value = dbget("select res_guest_status from reservation.res_reservation \
-                       where res_id='"+res_id+"' and res_guest_status in('Cancel') ")
+                       where res_id='"+res_id+"' and res_guest_status in('cancel') ")
     res_status = json.loads(sql_value)
-    print(res_status)
-    print(res_status[0]['res_guest_status'],type(res_status[0]['res_guest_status']))
+    #print(res_status)
+    #print(res_status[0]['res_guest_status'],type(res_status[0]['res_guest_status']))
     e['res_id'] = res_id
  
     s['res_guest_status'] = "Reserved"
