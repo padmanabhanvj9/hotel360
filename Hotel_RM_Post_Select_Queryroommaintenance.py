a382bf571448d8ae52bfadff4695155ef317cbbf
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 12:19:47 +0530

    Update Hotel_RM_Post_Select_Queryroommaintenance.py

--- Hotel_RM_Post_Select_Queryroommaintenance.py
+++ Hotel_RM_Post_Select_Queryroommaintenance.py
@@ -1,6 +1,6 @@
-from sqlwrapper import dbget,dbfetch
+from sqlwrapper import dbget,dbput
 import json
-from flask import Flask,request, jsonify
+
 
 def hotel_rm_post_select_queryroommaintenance(request):
     sql = json.loads(dbget("select * from room_management.rm_room_mainteanance_acitivity_log join \
@@ -9,4 +9,8 @@ def hotel_rm_post_select_queryroommaintenance(request):
                             room_management.rm_room_list.rm_room"))
     print(sql)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql  ,'ReturnCode':'RRTS'},indent=4))
-    
+def hotel_rm_post_delete_deleteroommaintenance(request):
+    room = request.json['rm_room']
+    sql = dbput("delete from room_management.rm_room_mainteanance_acitivity_log where rm_room='"+room+"'")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Deleted Successfully','ReturnCode':'RDS'}, sort_keys=True, indent=4))
+
