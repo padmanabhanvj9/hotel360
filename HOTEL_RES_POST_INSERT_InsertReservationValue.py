144c6754673c271efa929179c04c3f9e8eb04fa4
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 13:32:36 +0530

    Update HOTEL_RES_POST_INSERT_InsertReservationValue.py

--- HOTEL_RES_POST_INSERT_InsertReservationValue.py
+++ HOTEL_RES_POST_INSERT_InsertReservationValue.py
@@ -71,3 +71,8 @@ def Hotel_RES_POST_INSERT_Cardtype(request):
     print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
 
+def Hotel_RES_POST_INSERT_Waitlist_reason(request):
+    d = request.json
+    sql_value = gensql('insert','reservation.waitlist_reason',d)
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'},indent=4))
