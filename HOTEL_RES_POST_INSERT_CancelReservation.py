62ff84a4fa0c6d6615e3bbb9b9f08f49daf15ce6
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 11:29:57 +0530

    Update HOTEL_RES_POST_INSERT_CancelReservation.py

--- HOTEL_RES_POST_INSERT_CancelReservation.py
+++ HOTEL_RES_POST_INSERT_CancelReservation.py
@@ -40,5 +40,5 @@ def HOTEL_RES_POST_INSERT_CancelReservation(request):
     s['Res_id'] = res_id
     
     sql_value = gensql('insert','reservation.res_activity_log',s)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Cancelled Successfully','cancellationNumber':cancel_id,'ReturnCode':'RCS'}, sort_keys=True, indent=4))
     
