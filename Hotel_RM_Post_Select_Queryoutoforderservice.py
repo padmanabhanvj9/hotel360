f164db83a7fd2f5f835394b97e49e35a6b60f4ad
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-20 12:20:23 +0530

    Update Hotel_RM_Post_Select_Queryoutoforderservice.py

--- Hotel_RM_Post_Select_Queryoutoforderservice.py
+++ Hotel_RM_Post_Select_Queryoutoforderservice.py
@@ -1,8 +1,13 @@
-from sqlwrapper import dbget,dbfetch
+from sqlwrapper import dbget,dbput
 import json
 
 def hotel_rm_post_select_queryoutoforderservice(request):
     sql = json.loads(dbget("select * from room_management.rm_room_oo"))
     print(sql)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql  ,'ReturnCode':'RRTS'},indent=4))
-    
+
+def hotel_rm_post_delete_deleteoutoforderservice(request):
+    room = request.json['rm_room']
+    sql = dbput("delete from room_management.rm_room_oo where rm_room='"+room+"'")
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Deleted Successfully','ReturnCode':'RDS'}, sort_keys=True, indent=4))
+
