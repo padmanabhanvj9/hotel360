a452833b6302eb2e3ab8ec62558fb6628ba9c735
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-06-26 18:19:18 +0530

    Update HOTEL_RES_POST_INSERT_UpdateDeposit.py

--- HOTEL_RES_POST_INSERT_UpdateDeposit.py
+++ HOTEL_RES_POST_INSERT_UpdateDeposit.py
@@ -3,6 +3,6 @@ import json
 def HOTEL_RES_POST_INSERT_UpdateDeposit(request):
     d = request.json
     sql_value = ('insert','reservation.res_deposit',d)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'CS'}, sort_keys=True, indent=4))
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 	
