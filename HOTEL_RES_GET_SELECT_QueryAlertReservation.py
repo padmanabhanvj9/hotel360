bb57a9be4f6a2f6a9bbd6b8af29aa6315bf948de
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-03 23:26:41 +0530

    Update HOTEL_RES_GET_SELECT_QueryAlertReservation.py

--- HOTEL_RES_GET_SELECT_QueryAlertReservation.py
+++ HOTEL_RES_GET_SELECT_QueryAlertReservation.py
@@ -6,10 +6,12 @@ def HOTEL_RES_GET_SELECT_QueryAlertReservation(request):
     
     
     Res_id = request.json['Res_id']
+    Res_unique_id = request.json['Res_unique_id']
     d = {}
   
   
     d['Res_id'] = Res_id
+    d['Res_unique_id'] = Res_unique_id
     sql_value = gensql('select','reservation.res_alert','*',d)
     sql_value = json.loads(sql_value)
     print(sql_value)
