47c1f9ceaeb1114d84494d35c9899a4e9c600e75
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-04 15:18:28 +0530

    Update HOTEL_RES_POST_INSERT_WaitlistReason.py

--- HOTEL_RES_POST_INSERT_WaitlistReason.py
+++ HOTEL_RES_POST_INSERT_WaitlistReason.py
@@ -3,7 +3,7 @@ import json
 
 def HOTEL_RES_POST_INSERT_WaitlistReason(request):
     d = request.json
-
     sql_value = gensql('insert','reservation.res_waitlist',d)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Waitlist Reason Assigned Successfully','ReturnCode':'WRAS'}, sort_keys=True, indent=4))
+    print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
    
