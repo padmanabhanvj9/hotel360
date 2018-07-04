f7890496ae8e1f4e8a88195d498b81bad7b3be1b
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-04 15:19:01 +0530

    Update HOTEL_RES_POST_UPDATE_UpdateWaitlistReservation.py

--- HOTEL_RES_POST_UPDATE_UpdateWaitlistReservation.py
+++ HOTEL_RES_POST_UPDATE_UpdateWaitlistReservation.py
@@ -4,9 +4,9 @@ import json
 
 def HOTEL_RES_POST_UPDATE_UpdateWaitlistReservation(request):
     d = request.json
-    a = { k : v for k,v in d.items() if v != '' if k not in ('RES_Id','waitlist')}
+    a = { k : v for k,v in d.items() if v != '' if k not in ('RES_Id','waitlist','Res_unique_id')}
     print(a)
-    e = { k : v for k,v in d.items() if k != '' if k in ('RES_Id','waitlist')}
+    e = { k : v for k,v in d.items() if k != '' if k in ('RES_Id','waitlist','Res_unique_id')}
     print(e)
     
     sql_value = gensql('update','reservation.res_waitlist',a,e)
