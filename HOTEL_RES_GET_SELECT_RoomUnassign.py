456fe741b45b6cd2d215128f1049e234b22f0f08
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-05 18:30:15 +0530

    Update HOTEL_RES_GET_SELECT_RoomUnassign.py

--- HOTEL_RES_GET_SELECT_RoomUnassign.py
+++ HOTEL_RES_GET_SELECT_RoomUnassign.py
@@ -8,15 +8,18 @@ def HOTEL_RES_GET_SELECT_RoomUnassign(request):
     Res_id  = Res_id.split(',')
 
     Res_id = str(Res_id)[1:-1]
+    Res_unique_id = request.json['Res_unique_id']
+    Res_unique_id  = Res_unique_id.split(',')
 
-    sqlvalue = json.loads(dbget("select res_room from reservation.res_reservation where res_id in ("+Res_id+")"))
+    Res_unique_id = str(Res_unique_id)[1:-1]
+    sqlvalue = json.loads(dbget("select res_room from reservation.res_reservation where res_id in ("+Res_id+") and res_unique_id in ("+Res_unique_id+")"))
     print("sqlval",sqlvalue)
     rooms = ''
     for  i in sqlvalue:
         if len(rooms) == 0:
-            rooms += "'"+i['res_room']+"'"
+            rooms += "'"+str(i['res_room'])+"'"
         else:
-            rooms += ","+"'"+i['res_room']+"'"
+            rooms += ","+"'"+str(i['res_room'])+"'"
     print(rooms)        
     
     fo_status = "vaccant"
@@ -24,9 +27,9 @@ def HOTEL_RES_GET_SELECT_RoomUnassign(request):
     psql = dbput("update room_management.rm_room_list set rm_fo_status = '"+fo_status+"',rm_reservation_status = '"+res_status+"',rm_fo_person = '0' where rm_room in ("+rooms+")")
     print(psql)
     data = '0'
-    sql = dbput("update reservation.res_reservation set res_room = "+data+" where res_id in ("+Res_id+") ")
+    sql = dbput("update reservation.res_reservation set res_room = "+data+" where res_id in ("+Res_id+") and res_unique_id in ("+Res_unique_id+") ")
 
     print(sql)
 
     
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':'Room UnAssigned Successfully'  ,'ReturnCode':'RUAS'},indent=4))
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':'Record Updated Successfully'  ,'ReturnCode':'RUS'},indent=4))
