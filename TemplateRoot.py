beca95ee678bbceacad3e91f7e3017638bb95d06
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-05 12:45:35 +0530

    Update TemplateRoot.py

--- TemplateRoot.py
+++ TemplateRoot.py
@@ -115,6 +115,7 @@ from HOTEL_RES_POST_DELETE_DeleteReservation import HOTEL_RES_POST_DELETE_Delete
 from Hotel_RES_Post_Update_TracesResloved import Hotel_RES_Post_Update_TracesResloved
 from HOTEL_RES_POST_UPDATE_RoomMove import HOTEL_RES_POST_UPDATE_RoomMove
 from HOTEL_RES_POST_INSERT_ReservationCreditcard import Hotel_RES_POST_Delete_DeleteReservationCreditcard
+from HOTEL_RES_POST_INSERT_UpdateDeposit import HOTEL_RES_POST_SELECT_QueryDeposit
 
 from HOTEL_RES_GET_SELECT_QueryReservationValue import Hotel_RES_GET_SELECT_Restype
 from HOTEL_RES_GET_SELECT_QueryReservationValue import Hotel_RES_GET_SELECT_Alertarea
@@ -499,6 +500,9 @@ def HOTEL_RES_POST_UPDATE_RoomMoveval():
 @app.route("/Hotel_RES_POST_Delete_DeleteReservationCreditcard",methods=['POST'])
 def Hotel_RES_POST_Delete_DeleteReservationCreditcardva():
     return Hotel_RES_POST_Delete_DeleteReservationCreditcard(request)
+@app.route("/HOTEL_RES_POST_SELECT_QueryDeposit",methods=['POST'])
+def HOTEL_RES_POST_SELECT_QueryDepositva():
+    return HOTEL_RES_POST_SELECT_QueryDeposit(request)
    
    
 @app.route("/Hotel_RES_POST_INSERT_RestypeInsert",methods=['POST'])
