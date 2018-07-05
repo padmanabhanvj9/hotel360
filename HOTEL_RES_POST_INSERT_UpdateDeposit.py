febdf1485c3dbe54fd8e869aeb599671e4dfa516
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-05 12:51:43 +0530

    Update HOTEL_RES_POST_INSERT_UpdateDeposit.py

--- HOTEL_RES_POST_INSERT_UpdateDeposit.py
+++ HOTEL_RES_POST_INSERT_UpdateDeposit.py
@@ -6,3 +6,11 @@ def HOTEL_RES_POST_INSERT_UpdateDeposit(request):
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 	
+def HOTEL_RES_POST_SELECT_QueryDeposit(request):
+    d = request.json
+    sql_value = gensql('select','reservation.res_deposit','*',d)
+    sql_value1 = json.loads(sql_value)
+    print(sql_value1)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value1  ,'ReturnCode':'RRTS'},indent=4))
+
+   
