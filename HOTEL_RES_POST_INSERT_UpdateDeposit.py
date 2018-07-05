d0c68c7a78264dc1f92abecb35ef26b304cec0b0
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-05 19:06:10 +0530

    Update HOTEL_RES_POST_INSERT_UpdateDeposit.py

--- HOTEL_RES_POST_INSERT_UpdateDeposit.py
+++ HOTEL_RES_POST_INSERT_UpdateDeposit.py
@@ -2,7 +2,7 @@ from sqlwrapper import gensql
 import json
 def HOTEL_RES_POST_INSERT_UpdateDeposit(request):
     d = request.json
-    sql_value = ('insert','reservation.res_deposit',d)
+    sql_value = gensql('insert','reservation.res_deposit',d)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 	
