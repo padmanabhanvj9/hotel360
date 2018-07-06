f176b67cd6ab0fb4bc92a0c0cc6f39e2afd73bb4
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-06 16:49:24 +0530

    Update HOTEL_FD_POST_UPDATE_CheckinGuestArrivals.py

--- HOTEL_FD_POST_UPDATE_CheckinGuestArrivals.py
+++ HOTEL_FD_POST_UPDATE_CheckinGuestArrivals.py
@@ -6,6 +6,7 @@ def HOTEL_FD_POST_UPDATE_CheckinGuestArrivals(request):
     
    d = request.json
    res_id = d.get("Res_id")
+   unique_id = d.get("Res_unique_id")
    pf_id = d.get("pf_id")
    a = {}
    RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
@@ -14,7 +15,7 @@ def HOTEL_FD_POST_UPDATE_CheckinGuestArrivals(request):
    RES_Log_Date = datetime.datetime.utcnow().date()
    print(RES_Log_Date)
    RES_Log_Date = str(RES_Log_Date)
-   arrival = dbget("select res_arrival, res_adults,res_room from reservation.res_reservation where res_id = '"+res_id+"' and pf_id = '"+pf_id+"'")
+   arrival = dbget("select res_arrival, res_adults,res_room from reservation.res_reservation where res_id = '"+res_id+"' and pf_id = '"+pf_id+"' and res_unique_id = '"+unique_id+"'")
   
    arrival = json.loads(arrival)
    print(arrival)
@@ -28,17 +29,19 @@ def HOTEL_FD_POST_UPDATE_CheckinGuestArrivals(request):
    if RES_Log_Date == today_arrival:
        p = {}
        p['res_id'] = res_id
+       p['res_unique_id'] = unique_id
        sql_value = gensql('select','room_management.rm_queue_room','rm_queue',p)
        sql_value = json.loads(sql_value)
  
        if len(sql_value) != 0:
-          psql = dbput("delete from room_management.rm_queue_room where res_id = '"+res_id+"'")
+          psql = dbput("delete from room_management.rm_queue_room where res_id = '"+res_id+"' and res_unique_id = '"+unique_id+"'")
           print(psql)
        else:
           pass
        e = {}
        e['Res_id'] = res_id
        e['pf_id'] = pf_id
+       e['res_unique_id'] = unique_id
        a['Res_guest_status'] = "checkin"
        
        sql_value = gensql('update','reservation.res_reservation',a,e)
@@ -58,9 +61,9 @@ def HOTEL_FD_POST_UPDATE_CheckinGuestArrivals(request):
        sql_value = gensql('insert','reservation.res_activity_log',s)
        fo_status = "occupied"
        res_status = "checkin"
-       sql_value = dbput("update room_management.rm_room_list set rm_fo_status = '"+fo_status+"',rm_reservation_status = '"+res_status+"',rm_fo_person = "+str(adult)+" where rm_room in ("+room+")")
+       sql_value = dbput("update room_management.rm_room_list set rm_fo_status = '"+fo_status+"',rm_reservation_status = '"+res_status+"',rm_fo_person = "+str(adult)+" where rm_room in ("+str(room)+")")
        
        print(sql_value)
-       return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Reservation Checkin Successfully','ReturnCode':'RCIS'}, sort_keys=True, indent=4))
+       return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
    else:
       return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Checkin a Today Guest arrivals only','ReturnCode':'CTG'}, sort_keys=True, indent=4))
