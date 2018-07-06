5f4b1d9ad5b4e6fcee57ec6b763a4d941fda8582
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-06 16:48:50 +0530

    Update HOTEL_RES_POST_UPDATE_RoomMove.py

--- HOTEL_RES_POST_UPDATE_RoomMove.py
+++ HOTEL_RES_POST_UPDATE_RoomMove.py
@@ -4,12 +4,13 @@ def HOTEL_RES_POST_UPDATE_RoomMove(request):
     d = request.json
     res_id = d.get("Res_id")
     res_room = d.get("Res_room")
+    res_unique_id = d.get("Res_unique_id")
     sql_value = dbget("select res_room,res_adults from reservation.res_reservation \
-                       where res_id = '"+res_id+"'")
+                       where res_id = '"+res_id+"' and res_unique_id = '"+res_unique_id+"' ")
     
     sql_value = json.loads(sql_value)
     print(sql_value)
-    rm_room = sql_value[0]['res_room']
+    rm_room = str(sql_value[0]['res_room'])
     adult = str(sql_value[0]['res_adults'])
     print(adult,type(adult))
     print(rm_room,type(rm_room))
@@ -19,15 +20,17 @@ def HOTEL_RES_POST_UPDATE_RoomMove(request):
     print(rm_room)
     fo_status = "vaccant"
     res_status = "not reserved"
+    #rm_hk_status = "vaccant"
     psql = dbput("update room_management.rm_room_list set rm_fo_status = '"+fo_status+"',rm_reservation_status = '"+res_status+"',rm_fo_person = '0' where rm_room in ("+rm_room+")")
     print(psql)
     if len(sql_value) != 0:
           data = '0'
-          psql = dbput("update reservation.res_reservation set res_room='"+data+"' where res_id = '"+res_id+"'")
+          psql = dbput("update reservation.res_reservation set res_room='"+data+"' where res_id = '"+res_id+"' and res_unique_id = '"+res_unique_id+"' ")
           print(psql)
           a,e = {},{}
           e['Res_id'] = res_id
           a['Res_room'] = res_room
+          e['Res_unique_id'] = res_unique_id
           sql_value = gensql('update','reservation.res_reservation',a,e)
           rmove_room = a.get("Res_room")
           print(rmove_room)
@@ -38,8 +41,9 @@ def HOTEL_RES_POST_UPDATE_RoomMove(request):
           
           fo_status = "occupied"
           res_status = "checkin"
+          #rm_hk_status = "occupied"
           sql_value = dbput("update room_management.rm_room_list set rm_fo_status = '"+str(fo_status)+"',rm_reservation_status = '"+res_status+"',rm_fo_person = '"+adult+"' where rm_room in ("+rmove_room+")")
           print(sql_value)
           room_move = "Room Move from "+ rm_room + "to "+rmove_room
-          return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Room Moved Successfully','RoomMove':room_move ,'ReturnCode':'RMS'}, sort_keys=True, indent=4))
+          return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','RoomMove':room_move ,'ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
