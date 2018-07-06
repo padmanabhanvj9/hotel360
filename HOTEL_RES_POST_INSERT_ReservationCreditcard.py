712a2b6ba71117d2be65fb019ef1994eca76c14b
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-06 15:12:55 +0530

    Update HOTEL_RES_POST_INSERT_ReservationCreditcard.py

--- HOTEL_RES_POST_INSERT_ReservationCreditcard.py
+++ HOTEL_RES_POST_INSERT_ReservationCreditcard.py
@@ -7,9 +7,9 @@ def HOTEL_RES_POST_INSERT_ReservationCreditcard(request):
 def Hotel_RES_Post_Update_UpdateReservationCreditcard(request):
     d = request.json
     print(d)
-    a = { k : v for k,v in d.items() if v != '' if k not in ('res_id','pf_id','cc_id')}
+    a = { k : v for k,v in d.items() if v != '' if k not in ('pf_id','cc_id')}
     print(a)
-    e = { k : v for k,v in d.items() if k != '' if k in ('res_id','pf_id','cc_id')}
+    e = { k : v for k,v in d.items() if k != '' if k in ('pf_id','cc_id')}
     print(e)
     sql_value = gensql('update','profile.pf_creditcard',a,e)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
@@ -27,13 +27,13 @@ def Hotel_RES_Get_Select_QueryReservationCreditcard(request):
 def Hotel_RES_POST_Delete_DeleteReservationCreditcard(request):
 
      e = {}
-     res_id = request.json['res_id']
+     #res_id = request.json['res_id']
      pf_id = request.json['pf_id']
      cc_id = request.json['cc_id']
      cc_id  = cc_id.split(',')
 
      cc_id = str(cc_id)[1:-1]
-     sql_value = dbput("delete from profile.pf_creditcard where res_id = '"+res_id+"' and pf_id = '"+pf_id+"' and cc_id in ("+cc_id+")")
+     sql_value = dbput("delete from profile.pf_creditcard where  pf_id = '"+pf_id+"' and cc_id in ("+cc_id+")")
      print(sql_value)
      return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':'Record Deleted Successfully'  ,'ReturnCode':'RDS'},indent=4))
 
