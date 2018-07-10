3a3d8184c553f55f80365945eed769c900eda31f
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-10 11:42:18 +0530

    Update HOTEL_FD_GET_SELECT_QueryQueueReservation.py

--- HOTEL_FD_GET_SELECT_QueryQueueReservation.py
+++ HOTEL_FD_GET_SELECT_QueryQueueReservation.py
@@ -3,9 +3,9 @@ import json
 def HOTEL_FD_POST_SELECT_QueryQueueReservation(request):
     sql = json.loads(dbget("select * from reservation.res_reservation join   room_management.rm_queue_room on \
                             reservation.res_reservation.res_id = room_management.rm_queue_room.res_id"))
-    #print(sql,type(sql),len(sql))
+    
     a,b,c = {},0,0
-    #print(b)
+    
     for i in sql:
 
         b += i['res_number_of_rooms']
@@ -20,19 +20,17 @@ def HOTEL_FD_POST_SELECT_QueryQueueReservation(request):
  
         a[""+str(i['res_room_type'])+""] = i['res_number_of_rooms']
 
-    #print("available",a,type(a))
     k={}
+    l=[]
     for j in a.items():
-        #print(j,type(j))
-        #print(j[0])
-        #print(j[1])
-        k['room_type'+str(c)] = j[0]
-        k['room_type_totel'+str(c)] = j[1]
-        c+=1
-    print(k,type(k))
-        #print(j.keys())
-        #print(j.values())
-    #print("total",b)    
-      
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql,'TotalRoomsinQueue':b,'overview':k,'ReturnCode':'RRTS'},indent=4))  
-    #return("hi")
+        k['room_type'] = j[0]
+        #l.append(k)
+        #print(l)
+        k['room_type_totel'] = j[1]
+        l.append(k)
+        #print(l)
+        #c+=1
+    #print(k,type(k))
+    print(l)
+    #k = json.loads(str(k)) 
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql,'TotalRoomsinQueue':b,'overview':l,'ReturnCode':'RRTS'},indent=4))  
