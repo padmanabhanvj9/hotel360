78d2e85fba8eed1c5838464083cf33f05ea6c778
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-17 13:28:01 +0530

    Update HOTEL_CAH_GET_SELECT_QUERYGUESTBILLING.py

--- HOTEL_CAH_GET_SELECT_QUERYGUESTBILLING.py
+++ HOTEL_CAH_GET_SELECT_QUERYGUESTBILLING.py
@@ -4,15 +4,15 @@ from sqlwrapper import gensql,dbget,dbput
 import datetime
 def HOTEL_CAH_POST_SELECT_QUERYGUESTBILLING(request):
     d = request.json
-    print(d)
+    #print(d)
     res_data = json.loads(gensql('select','reservation.res_reservation',' res_guest_status,res_arrival, res_depature, res_rate_code, res_rate, \
                           res_adults, res_room_type,pf_id',d))
-    print(res_data)
+    #print(res_data)
     if res_data[0]['pf_id'][0:3] == 'cpy':
-        print(res_data[0]['pf_id'])
+        #print(res_data[0]['pf_id'])
         profile_data = json.loads(dbget("select pf_account from profile.pf_company_profile where pf_id='cpy101'"))
         res_data[0]['company'] = profile_data[0]['pf_account']
-    print(res_data)    
+    #print(res_data)    
     billing_data = json.loads(dbget("select billing_post.posting_date,billing_code.posting_code,billing_code.posting_code_description,\
                           billing_post.posting_amount,billing_post.post_window_id \
                           from cashiering.billing_post  join cashiering.billing_code on cashiering.billing_post.post_code_id = \
