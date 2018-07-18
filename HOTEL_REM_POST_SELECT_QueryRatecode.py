c926b36491c24503eaa7665f32f846fc896fdfc1
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-18 11:43:39 +0530

    Update HOTEL_REM_POST_SELECT_QueryRatecode.py

--- HOTEL_REM_POST_SELECT_QueryRatecode.py
+++ HOTEL_REM_POST_SELECT_QueryRatecode.py
@@ -3,10 +3,15 @@ import datetime
 import json
 def HOTEL_REM_POST_SELECT_QueryRatecode(request):
   res_arrival = request.json['res_arrival']
-  sql_value = dbget("select ratecode_setup.*, ratecode.rate_code, room_type.type, rate_details.*, currency.currency from revenue_management.ratecode_setup left join revenue_management.ratecode \
-                 on revenue_management.ratecode_setup.ratecode_id = revenue_management.ratecode.ratecode_id left join room_management.room_type \
-                 on room_management.room_type.id = revenue_management.ratecode_setup.roomtype_id left join revenue_management.currency \
-                 on revenue_management.currency.transaction_currency_id = revenue_management.ratecode_setup.transaction_currency_id left join revenue_management.rate_details \
-                 on revenue_management.rate_details.ratecode_id = revenue_management.ratecode_setup.ratecode_id where '"+res_arrival+"' between rate_details.start_date and  rate_details.end_date")
+  sql_value = dbget("select revenue_management.ratecode_setup.*,revenue_management.rate_details.*, room_management.room_type.*, \
+revenue_management.currency.*, revenue_management.ratecategory.*, revenue_management.negotiated_rate.*,revenue_management.season_code.* \
+from revenue_management.ratecode_setup join revenue_management.rate_details \
+on revenue_management.ratecode_setup.ratecode_id = revenue_management.rate_details.ratecode_id \
+join room_management.room_type on revenue_management.ratecode_setup.roomtype_id = room_management.room_type.id \
+join revenue_management.currency on revenue_management.ratecode_setup.transaction_currency_id = revenue_management.currency.transaction_currency_id \
+join revenue_management.ratecategory on revenue_management.ratecode_setup.rate_category_id = revenue_management.ratecategory.ratecategory_id \
+join revenue_management.negotiated_rate on revenue_management.ratecode_setup.ratecode_id = revenue_management.negotiated_rate.negotiated_code_id \
+join revenue_management.season_code on revenue_management.rate_details.season_code_id = revenue_management.season_code.season_code_id \
+where '"+res_arrival+"' between revenue_management.rate_details.start_date and revenue_management.rate_details.end_date")
   sqlvalue = json.loads(sql_value)
   return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sqlvalue  ,'ReturnCode':'RRTS'},indent=4))
