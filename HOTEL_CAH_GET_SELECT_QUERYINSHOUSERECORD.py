a788e5273fefe713bb2e2893be783807e7034b84
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-25 17:18:38 +0530

    Update HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py

--- HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
+++ HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
@@ -4,6 +4,6 @@ import json
 
 def HOTEL_CAH_POST_SELECT_QUERYINHOUSERECORD(request):
 
-    sql_value = json.loads(dbget("SELECT * FROM reservation.res_reservation where res_guest_status in ('checkin','due in') "))
+    sql_value = json.loads(dbget("SELECT * FROM reservation.res_reservation where res_guest_status in ('checkin','due out') "))
     #print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value  ,'ReturnCode':'RRTS'},indent=4))
