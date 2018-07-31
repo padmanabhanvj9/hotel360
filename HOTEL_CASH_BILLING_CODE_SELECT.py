121811616cd2d6f0280192a9d0d4b630bba2ffbc
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-31 16:56:16 +0530

    Update HOTEL_CASH_BILLING_CODE_SELECT.py

--- HOTEL_CASH_BILLING_CODE_SELECT.py
+++ HOTEL_CASH_BILLING_CODE_SELECT.py
@@ -3,7 +3,7 @@ from sqlwrapper import gensql,dbget
 import datetime
 
 def HOTEL_CASH_BILLING_CODE_SELECT(request):
-    s = dbget("select * from cashiering.billing_code")
+    s = json.loads(dbget("select * from cashiering.billing_code"))
     print(s)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':s  ,'ReturnCode':'RRTS'},indent=4))
 
