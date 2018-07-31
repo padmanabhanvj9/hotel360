e39883da3d7704975fa9ec8a235684363b7803c5
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-07-31 16:48:43 +0530

    Update HOTEL_CASH_BILLING_CODE_SELECT.py

--- HOTEL_CASH_BILLING_CODE_SELECT.py
+++ HOTEL_CASH_BILLING_CODE_SELECT.py
@@ -5,5 +5,5 @@ import datetime
 def HOTEL_CASH_BILLING_CODE_SELECT(request):
     s = dbget("select * from cashiering.billing_code")
     print(s)
-    return s
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':s  ,'ReturnCode':'RRTS'},indent=4))
 
