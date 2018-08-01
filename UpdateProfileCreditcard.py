1c7d8ff6a4d69850b000df93a0ce157b9504559e
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-01 12:50:53 +0530

    Update UpdateProfileCreditcard.py

--- UpdateProfileCreditcard.py
+++ UpdateProfileCreditcard.py
@@ -1,10 +1,15 @@
-from sqlwrapper import gensql
+from sqlwrapper import gensql,dbget
 import json
 
 def UpdateProfileCreditcard(request):
     d = request.json
-    sql_value = gensql('insert','profile.pf_creditcard',d)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
+    #print(d)
+    gensql('insert','profile.pf_creditcard',d)
+    sql_value = json.loads(dbget("select cc_id from profile.pf_creditcard \
+                                   where pf_creditcard_no='"+d['PF_Creditcard_No']+"' \
+                                   and pf_expiration_date='"+d['PF_Expiration_Date']+"' and res_id='"+d['res_id']+"' "))
+    #print(sql_value)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','Return_value':sql_value[0]['cc_id'],'ReturnCode':'RIS'}, sort_keys=True, indent=4))
 
 
    
