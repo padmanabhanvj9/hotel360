5792740d83391439d7e02bc76f022b1293984010
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-01 17:22:53 +0530

    Update TemplateRoot.py

--- TemplateRoot.py
+++ TemplateRoot.py
@@ -513,7 +513,7 @@ def GuestTraces():
 @app.route("/Hotel_RES_Post_Update_UpdateGuestTraces",methods=['POST'])
 def UpdateGuestTraces():
     return Hotel_RES_Post_Update_UpdateGuestTraces(request)
-@app.route("/Hotel_RES_Get_Select_QueryGuestTraces",methods=['POST'])
+@app.route("/Hotel_RES_Get_Select_QueryGuestTraces",methods=['GET'])
 def SelectGuestTraces():
     return Hotel_RES_Get_Select_QueryGuestTraces()
 @app.route("/HOTEL_RES_POST_UPDATE_UpdateFixedChargesReservation",methods=['POST'])
