35c26621a8f4d270df4a0cadc72734c8a1070b16
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-02 19:28:58 +0530

    Update HOTEL_BBL_POST_INSERT_Grid.py

--- HOTEL_BBL_POST_INSERT_Grid.py
+++ HOTEL_BBL_POST_INSERT_Grid.py
@@ -2,6 +2,17 @@ from sqlwrapper import gensql
 import json
 def HOTEL_BBL_POST_INSERT_Grid(request):
     d = request.json
+    x = {}
+    for k,v in d.items():
+     print(k,v)
+     if v is '':
+          d[k] =  0
+          print(d)
+     else:
+          pass
+   
+    print(d)
+    
     sql2 = gensql('insert','business_block.grid',d)
     print(sql2)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
