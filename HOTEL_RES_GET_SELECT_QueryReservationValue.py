c741e63eb7533a9e518dd7098df78d51e3c5a441
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-02 22:21:37 +0530

    Update HOTEL_RES_GET_SELECT_QueryReservationValue.py

--- HOTEL_RES_GET_SELECT_QueryReservationValue.py
+++ HOTEL_RES_GET_SELECT_QueryReservationValue.py
@@ -78,8 +78,4 @@ def Hotel_RES_GET_SELECT_Waitlist_reason():
     print(result)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': result  ,'ReturnCode':'RRTS'},indent=4))
 
-def Hotel_RES_GET_SELECT_Block_status():
-    sql_value = gensql('select','business_block.block_status','*')
-    result = json.loads(sql_value)
-    print(result)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': result  ,'ReturnCode':'RRTS'},indent=4))
+
