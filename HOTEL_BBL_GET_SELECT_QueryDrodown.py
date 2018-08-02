9e1a2290fc79af0665eac4a8ea7b6fa884aff72e
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-02 22:14:25 +0530

    Add files via upload

---
+++ HOTEL_BBL_GET_SELECT_QueryDrodown.py
@@ -0,0 +1,23 @@
+import json
+from sqlwrapper import gensql,dbget
+
+def HOTEL_BBL_GET_SELECT_BusinessBlockStatus():
+    
+    sql_value = gensql('select','business_block.block_status','*')
+    result = json.loads(sql_value)
+    print(result)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': result  ,'ReturnCode':'RRTS'},indent=4))
+
+def HOTEL_BBL_GET_SELECT_InventoryContrtol():
+   
+    sql_value = gensql('select','business_block.inventory_control','*')
+    result = json.loads(sql_value)
+    print(result)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': result  ,'ReturnCode':'RRTS'},indent=4))
+
+def HOTEL_BBL_GET_SELECT_MeetingSpaceType():
+    
+    sql_value = gensql('select','business_block.meeting_space_type','*')
+    result = json.loads(sql_value)
+    print(result)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue': result  ,'ReturnCode':'RRTS'},indent=4))
