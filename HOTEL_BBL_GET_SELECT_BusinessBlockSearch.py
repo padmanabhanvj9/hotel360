7aa05243482de75b034741c6f894433dcca75e32
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-03 21:48:45 +0530

    Update HOTEL_BBL_GET_SELECT_BusinessBlockSearch.py

--- HOTEL_BBL_GET_SELECT_BusinessBlockSearch.py
+++ HOTEL_BBL_GET_SELECT_BusinessBlockSearch.py
@@ -6,13 +6,13 @@ def HOTEL_BBL_GET_SELECT_BusinessBlockSearch():
 
     s = json.loads(dbget("select   block_room.block_room_id,block_room.res_type_id,block_room.cutoff_date,block_room.cutoff_days, \
 			  block_room.inventory_control_id,block_room.ratecode_id,block_room.print_rate,block_room.suppress_rate,\
-			  block_room.packages,block_room.trace_code,\
+			  block_room.packages,block_room.trace_code,block_room.follow_date\
 			  business_block_definite.*,\
 			  block_business_details.business_details_id,block_business_details.res_method_id,block_business_details.payments_id,\
 			  block_business_details.rooming_list_duedate,block_business_details.arrivaltime,block_business_details.depaturetime,\
 			  block_business_details.commission,\
 			  block_catering.attendess_id,block_catering.guranteed,block_catering.item_discount_perc,block_catering.attendess,\
-			  block_catering.info_board,block_catering.contract_no,block_catering.onsite_name,block_catering.owner_id,\
+			  block_catering.info_board,block_catering.contract_no,block_catering.onsite_name,block_catering.followup_date\
 			  block_cancel_catering.catering_cancel_id,block_cancel_catering.catering_cancel_no,block_cancel_catering.catering_reason_id,\
 			  block_cancel_catering.catering_comments,block_cancel_catering.room_cancel_no,block_cancel_catering.room_cancel_reason,\
 			  block_cancel_catering.room_cancel_comments,block_cancel_catering.room_cancel_destination,\
