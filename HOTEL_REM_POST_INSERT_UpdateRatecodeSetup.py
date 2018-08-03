57cfecd4b05bb58283547b38c2bcadf01596e5e6
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-03 13:52:21 +0530

    Update HOTEL_REM_POST_INSERT_UpdateRatecodeSetup.py

--- HOTEL_REM_POST_INSERT_UpdateRatecodeSetup.py
+++ HOTEL_REM_POST_INSERT_UpdateRatecodeSetup.py
@@ -1,59 +1,59 @@
-from sqlwrapper import gensql, dbget
+from sqlwrapper import gensql, dbget, dbput
 import datetime
 import json
 def HOTEL_REM_POST_INSERT_UpdateRatecodeSetup(request):
     d = request.json
-    today_date = str(datetime.datetime.utcnow().date())
-    #end_sell_date = datetime.datetime.utcnow().date()
-    begin_date = d.get("Begin_sell_date")
-    print(begin_date,type(begin_date))
-    end_date = d.get("End_sell_date")
-    print(end_date, type(end_date))
-    roomtype = d.get("Roomtype_id")
-    print(roomtype)
-    no = roomtype.split(',')
-    print(no)
-    #roomtype = roomtype.replace(',','')
-    if today_date <= begin_date and begin_date <= end_date:
-     for i in no:
-       
-       print(i)
-       print('insert',i)
-       d['Roomtype_id'] = i     
-       gensql('insert','revenue_management.ratecode_setup',d)
-     ratecode = d.get("Ratecode_id")
-     rate = json.loads(dbget("select rate_description from revenue_management.ratecode where ratecode_id='"+ratecode+"'"))
-       #rate = str(json.loads([0]['rate_description']))
-     rate = rate[0]['rate_description']
-     print(rate)
-     roomtype = json.loads(dbget("select type from room_management.room_type where id in ("+roomtype+")"))
-     print(roomtype)
-     str1 = ''
-     for i in roomtype:
-           print(i)
-           if len(str1) == 0:  
-              str1 += str(i['type'])
-           else:
-              str1 += ','+ str(i['type'])
-     print('string',str1)
-     roomtype = str(roomtype[0]['type'])
-     print(roomtype)
-     begin_date = d.get("Begin_sell_date")
-     end_date = d.get("End_sell_date")
-     RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
-     RES_Log_Time = RES_Log_Time.time().strftime("%H:%M:%S")
-     print(RES_Log_Time)
-     RES_Log_Date = datetime.datetime.utcnow().date()
-     print(RES_Log_Date)
-     s = {}
-     s['date'] = RES_Log_Date
-     s['time'] = RES_Log_Time
-     s['emp_name'] = "Daisy"
-     s['action_type'] = "Rate setup for "+" "+rate
-     s['action_description'] = rate+" "+"allow for these roomtype"+" "+str1
-     s['rate_code_id'] = ratecode
-     gensql('insert','revenue_management.revenue_activity_log',s)
-    
-     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
-    else:
-         return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Rate Setup only for upcoming days not past date','ReturnCode':'RS'}, sort_keys=True, indent=4))   
+    #print(d)
+    e = { k : v for k,v in d.items() if k in ('begin_sell_date','end_sell_date','market_id','source_id','display_sequence')}
+    print("0000",e)
+    #rate_code_details = d['rate_code_details']
+    #print(rate_code_details)
+    #sell_controls = d['sell_controls']
+    #print(sell_controls)
+    #transaction_details = d['transaction_details']
+    #print(transaction_details)
+    #components = d['components']
+    #print(components)
+    #room_types = d['room_types']
+    #print("room_types",room_types,type(room_types))
+    #package = d['package']
+    #print("package",package,type(package))
+    gensql('insert','revenue_management.ratecode',d['rate_code_details'])
+    ratecode_id = json.loads(gensql('select','revenue_management.ratecode','ratecode_id ',d['rate_code_details']))
+    print(ratecode_id[0]['ratecode_id'])
+    e['ratecode_id'] = ratecode_id[0]['ratecode_id']
+    print("1111",e)
+    room_types = d['room_types']
+    room_id = json.loads(dbget("select max(rooms_id) as number1 from revenue_management.rooms_selected"))
+    print("room_id",room_id[0]['number1'])
+    for i in d['room_types']:
+        #pass
+        dbput("insert into revenue_management.rooms_selected (rooms_id,room_type_id) \
+               values ('"+str(room_id[0]['number1']+1)+"','"+str(i)+"') ")
+    e['rooms_id'] = int(room_id[0]['number1']+1)
+    print("2222",e)
+    package_id = json.loads(dbget("select max(packages_id) as number2 from revenue_management.packages_selected"))
+    print("package_id",package_id[0]['number2'],type(package_id[0]['number2']))
+    for j in d['package']:
+        #pass
+        dbput("insert into revenue_management.packages_selected (packages_id,package_code_id) \
+               values ('"+str(package_id[0]['number2']+1)+"','"+str(j)+"')")
+    e['packages_id'] =  int(package_id[0]['number2']+1)
+    print("3333",e)
+    gensql('insert','revenue_management.sell_control',d['sell_controls'])
+    sell_id = json.loads(gensql('select','revenue_management.sell_control','sell_id',d['sell_controls']))
+    print("sell_id",sell_id[0]['sell_id'])
+    e['sell_control_id'] = int(sell_id[0]['sell_id'])
+    print("4444",e)
+    gensql('insert','revenue_management.tranction_details',d['transaction_details'])
+    transaction_detail_id = json.loads(gensql('select','revenue_management.tranction_details','tranction_detail_id',d['transaction_details']))
+    print("transaction_detail_id",transaction_detail_id[0]['tranction_detail_id'])
+    e['transaction_details_id'] = int(transaction_detail_id[0]['tranction_detail_id'])
+    print("5555",e)
+    gensql('insert','revenue_management.rate_components',d['components'])
+    components_id = json.loads(gensql('select','revenue_management.rate_components','components_id',d['components']))
+    print("components_id",components_id[0]['components_id'])
+    e['rate_components_id'] = int(components_id[0]['components_id'])
+    print("6666",e)
+    gensql('insert','revenue_management.ratecode_setup',e)
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}, sort_keys=True, indent=4))
