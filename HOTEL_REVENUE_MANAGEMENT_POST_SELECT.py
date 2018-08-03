6c1f549a03fc01b591fe1be27853e8dad08b3535
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-03 13:50:54 +0530

    Update HOTEL_REVENUE_MANAGEMENT_POST_SELECT.py

--- HOTEL_REVENUE_MANAGEMENT_POST_SELECT.py
+++ HOTEL_REVENUE_MANAGEMENT_POST_SELECT.py
@@ -27,3 +27,12 @@ def currencyselect(request):
     print(s)
     return(json.dumps({"Return": s,"Status": "Success","StatusCode": "200"},indent=4))
 
+def room_types(request):
+    s = json.loads(dbget("select * from revenue_management.room_type"))
+    print(s)
+    return(json.dumps({"Return": s,"Status": "Success","StatusCode": "200"},indent=4))
+
+def packages_revenue(request):
+    s = json.loads(dbget("SELECT * FROM revenue_management.packages_codes"))
+    print(s)
+    return(json.dumps({"Return": s,"Status": "Success","StatusCode": "200"},indent=4))
