fc94557d75f7a5489ad329a9e94c7f27ca0cbea2
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-09 17:15:38 +0530

    Update HOTEL_BBL_POST_SELECT_QueryGrid.py

--- HOTEL_BBL_POST_SELECT_QueryGrid.py
+++ HOTEL_BBL_POST_SELECT_QueryGrid.py
@@ -6,7 +6,7 @@ def HOTEL_BBL_POST_SELECT_QueryGrid(request):
     sql = json.loads(dbget("select grid.*, room_management.room_type.type \
                 from business_block.grid \
                 join room_management.room_type on room_management.room_type.id = business_block.grid.roomtype_id \
-                where grid.block_id = '"+block_id+"'))
+                where grid.block_id = '"+block_id+"'"))
     print(sql)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql  ,'ReturnCode':'RRTS'},indent=4))
    
