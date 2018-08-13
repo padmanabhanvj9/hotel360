511adb375da740497d46f7afff429f9b76427bd0
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-13 12:29:20 +0530

    Update HOTEL_BBL_POST_SELECT_QueryGrid.py

--- HOTEL_BBL_POST_SELECT_QueryGrid.py
+++ HOTEL_BBL_POST_SELECT_QueryGrid.py
@@ -3,7 +3,17 @@ import json
 def HOTEL_BBL_POST_SELECT_QueryGrid(request):
     d = request.json
     block_id = d.get("block_id")
-    sql =  json.loads(gensql('select','business_block.current_grid','*',block_id))
+    sql =  json.loads(dbget("SELECT current_grid.*,grid_type_desc FROM business_block.current_grid join\
+                                   business_block.grid_type on current_grid.grid_type = grid_type.grid_type_id \
+                                   where block_id="+str(l['block_id'])+" "))
+    grid_data1 = []
+    for data in sql:
+      for k,v in data.items():
+         if v is None:
+           data[k] =  0        
+         else:
+           pass
+    grid_data1.append(data)
     print(sql)
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql  ,'ReturnCode':'RRTS'},indent=4))
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':grid_data1,'ReturnCode':'RRTS'},indent=4))
    
