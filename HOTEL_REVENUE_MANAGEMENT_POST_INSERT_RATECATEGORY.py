c6f1c95bb882620b82e8dbf877a4a3b271488481
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-15 12:35:18 +0530

    Update HOTEL_REVENUE_MANAGEMENT_POST_INSERT_RATECATEGORY.py

--- HOTEL_REVENUE_MANAGEMENT_POST_INSERT_RATECATEGORY.py
+++ HOTEL_REVENUE_MANAGEMENT_POST_INSERT_RATECATEGORY.py
@@ -38,31 +38,37 @@ def negotiated(request):
 
 def ratedetailss(request):
     d = request.json
-    print(d)
+    #print(d)
+    #print(d['rate_tier_id'])
+
+    
     e = { k : v for k,v in d.items() if k not in ('days','room_types','package')}
-    print(e)
+    #print(e)
     room_id = json.loads(dbget("select max(rooms_id) as number1 from revenue_management.rooms_selected"))
-    print("room_id",room_id[0]['number1'])    
+    #print("room_id",room_id[0]['number1'])    
     for i in d['room_types']:
         #pass
         dbput("insert into revenue_management.rooms_selected (rooms_id,room_type_id) \
                values ('"+str(room_id[0]['number1']+1)+"','"+str(i)+"') ")
     e['rooms_id'] = int(room_id[0]['number1']+1)
-    print("2222",e)
-    package_id = json.loads(dbget("select max(packages_id) as number2 from revenue_management.packages_selected"))
-    print("package_id",package_id[0]['number2'],type(package_id[0]['number2']))
-    for j in d['package']:
+    #print("2222",e)
+    if d['rate_tier_id'] != 0:
+            
+       package_id = json.loads(dbget("select max(packages_id) as number2 from revenue_management.packages_selected"))
+       print("package_id",package_id[0]['number2'],type(package_id[0]['number2']))
+       for j in d['package']:
         #pass
-        dbput("insert into revenue_management.packages_selected (packages_id,package_code_id) \
+         dbput("insert into revenue_management.packages_selected (packages_id,package_code_id) \
                values ('"+str(package_id[0]['number2']+1)+"','"+str(j)+"')")
-    e['packages_id'] =  int(package_id[0]['number2']+1)
-    print("3333",e)
+       e['packages_id'] =  int(package_id[0]['number2']+1)
+    #print("3333",e)
     gensql('insert','revenue_management.rate_days',d['days'])
     #days_id = json.loads(gensql('select','revenue_management.rate_days','rate_days_id',d['days']))
     days_id = json.loads(dbget("select max(rate_days_id) as number3 from revenue_management.rate_days"))
     print("days",days_id[0]['number3'])
     e['rate_days_id'] = int(days_id[0]['number3'])
-    print("4444",e)
+    #print("4444",e)
     gensql('insert','revenue_management.rate_details',e)
+    
     return(json.dumps({"Return": "Record Inserted Successfully","ReturnCode": "RIS","Status": "Success","StatusCode": "200"},indent=4))
            
