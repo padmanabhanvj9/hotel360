ef2b730411c90eff0f05a2ce19859c658e7d054b
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-16 17:25:57 +0530

    Update HOTEL_BBL_POST_SELECT_QueryGrid.py

--- HOTEL_BBL_POST_SELECT_QueryGrid.py
+++ HOTEL_BBL_POST_SELECT_QueryGrid.py
@@ -5,7 +5,7 @@ def HOTEL_BBL_POST_SELECT_QueryGrid(request):
     block_id = d.get("block_id")
     sql =  json.loads(dbget("SELECT current_grid.*,grid_type_desc FROM business_block.current_grid join\
                                    business_block.grid_type on current_grid.grid_type = grid_type.grid_type_id \
-                                   where block_id="+str(block_id)+" "))
+                                   where block_id="+str(block_id)+" order by current_grid_id "))
     grid_data1 = []
     for data in sql:
       for k,v in data.items():
