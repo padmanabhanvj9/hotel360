86d86248d0fc500d7e1d6292319f74ee5b915ae6
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-23 12:11:17 +0530

    Update HOTEL_PAC_POST_UPDATE_Package_Code.py

--- HOTEL_PAC_POST_UPDATE_Package_Code.py
+++ HOTEL_PAC_POST_UPDATE_Package_Code.py
@@ -7,7 +7,7 @@ def HOTEL_PAC_POST_UPDATE_Packages(request):
     x = { k : v for k,v in d.items() if k not in ('alternates','alternate_id',
                                                   'item_inventory_selected_id','item_id')}
     print("adsfasd",x)
-    '''    
+    
     dbput("delete from packages.item_inventory_selected where item_id='"+str(d['item_id'])+"' ")
     
     for i in d['item_inventory_selected_id']:
@@ -22,13 +22,17 @@ def HOTEL_PAC_POST_UPDATE_Packages(request):
         dbput("insert into packages.alternate_selected (alternate_id,package_code_id) \
                values ('"+str(d['alternate_id'])+"','"+str(i)+"') ")
        
-    '''
-    a = { k : v for k,v in x.items()  if k not in ('package_code_id')}
-    print("a.......",a)
-    b = { k : v for k,v in x.items()  if k  in ('package_code_id') }
     
+    d={}
+    a = { k : v for k,v in x.items()  if k in ('package_code_id')}
+    b = { k : v for k,v in x.items()  if k  not in ('package_code_id') }
+    #c = { k : v for k,v in b.items()  if k  not in ('package_code') }
+    print("a.......",a['package_code'])
+    b['package_code'] = a['package_code']
     print("b.......values",b)
-    #gensql('update','packages.package_code',a,b)
+    d['package_code_id'] = a['package_code_id']
+    print(d)
+    gensql('update','packages.package_code',b,d)
       
     return(json.dumps({'Status': 'Success', 'StatusCode': '200',
                        'Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4)) 
