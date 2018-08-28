c2d86a31ed9c5d624b2680ec51a753f3f4910c6a
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-28 12:12:23 +0530

    Update HOTEL_CAH_POST_UPDATE_UPDATEGUESTBILLING.py

--- HOTEL_CAH_POST_UPDATE_UPDATEGUESTBILLING.py
+++ HOTEL_CAH_POST_UPDATE_UPDATEGUESTBILLING.py
@@ -18,7 +18,10 @@ def HOTEL_CAH_POST_UPDATE_UPDATEGUESTBILLING(request):
 
     Posting_date = datetime.datetime.utcnow().date()
     Revenue_date = datetime.datetime.utcnow().date()
-
+    result = json.loads(dbget("select log_link_id from cashiering.log_link where link_id='1' "))
+    log_id = result[0]['log_link_id']+1
+    print(log_id)
+    dbput("update cashiering.log_link set log_link_id="+str(log_id)+" where link_id='1'")
     s = {}
     s['Posting_date'] = Posting_date
     s['Revenue_date'] = Revenue_date
@@ -28,6 +31,7 @@ def HOTEL_CAH_POST_UPDATE_UPDATEGUESTBILLING(request):
     s['Posting_action'] = "Night Audit posting"
     s['Posting_reason'] = "Payment posted for"+ " "+res_id
     s['Posting_description'] = "Payment posted successfully"
+    s['log_link_id'] = log_id
     gensql('insert','cashiering.posting_changes_history_log',s)
     gensql('insert','cashiering.posting_history_log',s)
 
