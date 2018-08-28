c7a26d2f3ceefbb308ef2d0c903ce3882d37a1eb
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-28 15:10:19 +0530

    Update HOTEL_PAC_POST_DELETE_Package_Code.py

--- HOTEL_PAC_POST_DELETE_Package_Code.py
+++ HOTEL_PAC_POST_DELETE_Package_Code.py
@@ -9,6 +9,6 @@ def HOTEL_PAC_POST_DELETE_Package(request):
 
 def HOTEL_PAC_POST_DELETE_Packagedetails(request):
     d = request.json
-    dbput("delete from packages.package_details where package_code_id = "+str(d['package_code_id'])+" ")
+    dbput("delete from packages.package_details where packages_details_id = "+str(d['packages_details_id'])+" ")
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Deleted Successfully','ReturnCode':'RDS'}, sort_keys=True, indent=4))
  
