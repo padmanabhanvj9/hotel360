4de1cd26e1d478da84296c6157642bdf5a37c650
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-08-28 16:05:37 +0530

    Update HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py

--- HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
+++ HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
@@ -4,6 +4,14 @@ import json
 
 def HOTEL_CAH_POST_SELECT_QUERYINHOUSERECORD(request):
 
-    sql_value = json.loads(dbget("SELECT * FROM reservation.res_reservation where res_guest_status in ('checkin','due out') "))
-    #print(sql_value)
+    value1 = json.loads(dbget("SELECT * FROM reservation.res_reservation where res_guest_status in ('checkin','due out') "))
+    print(value1) 
+    value2 = json.loads(dbget("select * from reservation.res_reservation where \
+                        CURRENT_DATE between res_arrival and res_depature and res_guest_status='Check out' "))  
+    print(value2)
+    sql_value = value1+value2
+    print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value  ,'ReturnCode':'RRTS'},indent=4))
+
+
+# select CURRENT_DATE) between res_arrival and res_depature and res_guest_status='Check out'
