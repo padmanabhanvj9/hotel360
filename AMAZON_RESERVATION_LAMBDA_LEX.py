49de99196a6b047c76341f4ad40e41c0c185ab49
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-09-18 10:41:13 +0530

    Update AMAZON_RESERVATION_LAMBDA_LEX.py

--- AMAZON_RESERVATION_LAMBDA_LEX.py
+++ AMAZON_RESERVATION_LAMBDA_LEX.py
@@ -3,5 +3,11 @@ import json
 from flask import Flask,request, jsonify
 def AMAZON_RESERVATION_LAMBDA_LEX(request):
     d = request.json
+    d['roomtype'] = 'KNGN'
+    d['adults'] = 2
+    d['child'] = 4
+    d['guestname'] = 'daisy'
+    d['phone'] = '9698689999'
+    d['email'] = 'veroni@gmail.com'
     sql_value = gensql('insert','amazonlex.reservation',d)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
