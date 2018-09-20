1c4bccbda8d6e971f4a3ed10836dfd34d74e8393
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-09-20 10:39:24 +0530

    Update AMAZON_RESERVATION_LAMBDA_LEX.py

--- AMAZON_RESERVATION_LAMBDA_LEX.py
+++ AMAZON_RESERVATION_LAMBDA_LEX.py
@@ -3,8 +3,23 @@ import json
 from flask import Flask,request, jsonify
 def AMAZON_RESERVATION_LAMBDA_LEX(request):
     d = {}
-   # arrivalsdate = request.args.get('arrival')
-    #depaturedate = request.args.get('depature')
+    arrivalsdate = request.args.get('arrival')
+    depaturedate = request.args.get('depature')
+    #arrivalsdate = request.json['arrival']
+    #depaturedate = request.json['depature']
+    d['arrival']  = arrivalsdate
+    d['depature']  = depaturedate
+    d['roomtype'] = 'KNGN'
+    d['adults'] = 2
+    d['child'] = 4
+    d['guestname'] = 'daisy'
+    d['phone'] = '9698689999'
+    d['email'] = 'veroni@gmail.com'
+    sql_value = gensql('insert','amazonlex.reservation',d)
+    #reservation = {'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RUS'}
+    return(json.dumps([{'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}], sort_keys=True, indent=4))
+def amazon_insert(request):
+    d = {}
     arrivalsdate = request.json['arrival']
     depaturedate = request.json['depature']
     d['arrival']  = arrivalsdate
@@ -16,5 +31,5 @@ def AMAZON_RESERVATION_LAMBDA_LEX(request):
     d['phone'] = '9698689999'
     d['email'] = 'veroni@gmail.com'
     sql_value = gensql('insert','amazonlex.reservation',d)
-    #reservation = {'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RUS'}
+    #reservation = {'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RIS'}
     return(json.dumps([{'Status': 'Success', 'StatusCode': '200','Return': 'Record Inserted Successfully','ReturnCode':'RUS'}], sort_keys=True, indent=4))
