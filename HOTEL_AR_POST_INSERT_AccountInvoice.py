db081bbf3c82f0ff517479e507d96432a0d7eef2
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-11-12 15:02:36 +0530

    Update HOTEL_AR_POST_INSERT_AccountInvoice.py

--- HOTEL_AR_POST_INSERT_AccountInvoice.py
+++ HOTEL_AR_POST_INSERT_AccountInvoice.py
@@ -115,6 +115,7 @@ def HOTEL_AR_POST_INSERT_Billingpayment(request):
     acc_inv = account_invoie[0]['open_amount'] - d['posting_amount']
     print(acc_inv)
     d['posting_date'] = Posting_date
+    d['posting_status'] = "Apply"
     gensql('insert','account_receivable.invoice_payment',d)
     sql = dbput("update account_receivable.account_setup set account_balance = '"+str(setupamount)+"' where account_number = '"+d['account_no']+"'")
     psql = dbput("update account_receivable.accout_inivoice set open_amount = '"+str(acc_inv)+"' where account_number = '"+d['account_no']+"'")
