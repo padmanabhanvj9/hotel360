f1373dc481cadf11e416d18fca192e497b9ed667
Padmanabhan K <padmanabhanvj9@gmail.com>
2018-11-30 19:08:48 +0530

    Update QueryProfileHistory.py

--- QueryProfileHistory.py
+++ QueryProfileHistory.py
@@ -13,22 +13,55 @@ def QueryProfileHistoryRecord(request):
 
 def QueryProfileStatistics(request):
     d = request.json['pf_id']
+    status,status_room,no_show_rooms =[],[],[]
+    
+    last_no_show_rooms,last_status,last_status_room = [],[],[]
     #print(d)
-    e = {}
-    #today = datetime.datetime.utcnow().date()
-    #print(today)
-    sql_value = json.loads(dbget("select * from reservation.res_reservation where pf_id = '"+d+"' "))
+    e ,s= {},{}
+    today = datetime.datetime.utcnow().date()
+    print(today)
+    print((today.year)-1)
+   
+    sql_value = json.loads(dbget("select * from reservation.res_reservation where pf_id = '"+d+"' and \
+                                  res_arrival between '"+str(today.year)+"-01-01' and '"+str(today.year)+"-12-31' "))
     #print(sql_value)
-    nights = 0
-    rooms = 0
+
+    current_year = getprofilestatistics(sql_value)
+    
+    
+    #Last yyear record###########################################################
+    sql_value = json.loads(dbget("select * from reservation.res_reservation where pf_id = '"+d+"' and \
+                                  res_arrival between '"+str((today.year)-1)+"-01-01' and '"+str((today.year)-1)+"-12-31' "))
+    last_year = getprofilestatistics(sql_value)
+   
+    
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','currentyear':current_year,'lastyear':last_year,'ReturnCode':'RRTS'},indent=4))
+def getprofilestatistics(sql_value):
+    e ,s= {},{}
+    status,status_room,no_show_rooms =[],[],[]
+    nights,rooms,rate = 0,0,0
     for i in sql_value:
+        arrival = datetime.datetime.strptime(i['res_arrival'],'%Y-%m-%d').date()
+      
         nights += int(i['res_nights'])
         rooms += int(i['res_number_of_rooms'])
-    print(nights,rooms)
+        rate += int(i['res_rate'])
+        
+        status.append(i['res_guest_status'])
+        if i['res_guest_status'] in 'cancel':
+            status_room.append(i['res_number_of_rooms'])
+            if i['res_guest_status'] in 'no show':
+              no_show_rooms.append(i['res_number_of_rooms'])
+    #print(status)
+    #print(nights,rooms,status_room)
     e['Room Nights'] = nights
     e['Arrival Rooms'] = rooms
-    
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':e  ,'ReturnCode':'RRTS'},indent=4))
+    e['Cancel Res'] = status.count("cancel")
+    e['Room Revenue'] = rate
+    e['No Show Res'] = status.count("no show")
+    e['Cancel Rooms'] = sum(i for i in status_room)
+    e['No Show Rooms'] = sum(i for i in no_show_rooms)
+    return(e)
 
 def QueryProfileFutureRecord(request):
     d = request.json['pf_id']
