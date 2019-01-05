e4f3f9fcc9773ddbbc25c8379230df0203f56644
iottech <iottech>
2019-01-05 13:26:44 +0530

    queuereservation

--- HOTEL_FD_GET_SELECT_QueryQueueReservation.py
+++ HOTEL_FD_GET_SELECT_QueryQueueReservation.py
@@ -1,38 +1,28 @@
 from sqlwrapper import gensql, dbget
 import json
+from collections import Counter
 def HOTEL_FD_POST_SELECT_QueryQueueReservation(request):
+    roomtype,l = [],[]
     sql = json.loads(dbget("select  rm_room_list.rm_room_status as roomstatus,rm_room_list.rm_fo_status as frontoffice, \
                             rm_room_list.rm_room_class as roomclass,rm_queue_room.rm_qtime,rm_queue_room.rm_queue,res_reservation.* from reservation.res_reservation \
 	                    join   room_management.rm_queue_room on \
                             reservation.res_reservation.res_unique_id = room_management.rm_queue_room.res_unique_id \
 			    join  room_management.rm_room_list on rm_room_list.rm_room = reservation.res_reservation.res_room"))
     
-    a,b,c = {},0,0
-    
+    a,b,c ,queue= {},0,0,0
+ #   list1 = [{'roomtype':k,'value':v} for k,v in Counter([i['res_room_type'] for i in sql).items()]
     for i in sql:
-
-        b += i['res_number_of_rooms']
-
-        if i['res_room_type'] in a :
-
-            val = a[""+i['res_room_type']+""]+i['res_number_of_rooms']
-
-            del a[""+i['res_room_type']+""]
-
-            a[""+str(i['res_room_type'])+""] = val
- 
-        a[""+str(i['res_room_type'])+""] = i['res_number_of_rooms']
-
-    k={}
-    l=[]
+       roomtype.append(i['res_room_type'])
+    print(roomtype)
+    print(Counter(roomtype))
+   
+   
     print(a)
-    for keys,values in a.items():
+    for keys,values in Counter(roomtype).items():
         print(keys,values)
-
+        queue += values
         l.append({'room_type':keys,'room_type_totel':values})
-        #print(l)
-        #c+=1
-    #print(k,type(k))
-    print(l)
-    #k = json.loads(str(k)) 
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql,'TotalRoomsinQueue':b,'overview':l,'ReturnCode':'RRTS'},indent=4))  
+       
+    print(queue)
+  
+    return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql,'TotalRoomsinQueue':queue,'overview':l,'ReturnCode':'RRTS'},indent=4))  
