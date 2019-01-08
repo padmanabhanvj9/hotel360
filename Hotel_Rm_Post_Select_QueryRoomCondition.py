80922579bda4490970a369d5d229f5f82c07df4c
iottech <iottech>
2019-01-08 11:37:31 +0530

    room condition

--- Hotel_Rm_Post_Select_QueryRoomCondition.py
+++ Hotel_Rm_Post_Select_QueryRoomCondition.py
@@ -2,8 +2,8 @@ from sqlwrapper import dbget,dbput
 import json
 
 def hotel_rm_post_select_queryroomcondition(request):
-    sql = json.loads(dbget('select room_management.rm_room_list.*,room_management.rm_room_condition.rm_condition from  room_management.rm_room_list  left join \
-                            room_management.rm_room_condition on room_management.rm_room_list.rm_room = \
+    sql = json.loads(dbget('select room_management.rm_room_list.*,room_management.rm_room_condition.rm_condition from  room_management.rm_room_condition  left join \
+                            room_management.rm_room_list on room_management.rm_room_list.rm_room = \
                             room_management.rm_room_condition.rm_room'))
     #print(sql)      
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql,'ReturnCode':'RRTS'},indent=4))
