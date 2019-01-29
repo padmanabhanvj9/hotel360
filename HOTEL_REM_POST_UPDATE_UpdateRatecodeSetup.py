a7a7c359c4cc1f953c4a5c61b55f619901a153a2
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-01-29 16:24:30 +0530

    update service change rate detials

--- HOTEL_REM_POST_UPDATE_UpdateRatecodeSetup.py
+++ HOTEL_REM_POST_UPDATE_UpdateRatecodeSetup.py
@@ -65,36 +65,65 @@ def HOTEL_REM_POST_UPDATE_Negotiated_Rate(request):
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
 
 def HOTEL_REM_POST_UPDATE_RATE_DETAILS(request):
+    
     d = request.json
     #print(d)
-    x = { k : v for k,v in d.items() if k in ('days')}
-    #print("x",x)
-    a = x['days']
-    #print("a",a)
-    b = { k : v for k,v in d.items() if k in ('rate_days_id') if k not in ('days')}
-    #print("b",b)
-    gensql('update','revenue_management.rate_days',a,b)
 
-    dbput("delete from revenue_management.rooms_selected where rooms_id='"+str(d['rooms_id'])+"' ")
+    dbput("delete from revenue_management.rate_details where rate_details_id='"+str(d['rate_details_id'])+"';  \
+	   delete from revenue_management.rates_all where rate_details_id = '"+str(d['rate_details_id'])+"'")
+        
+    e = { k : v for k,v in d.items() if k not in ('days','room_types','package','start_date',
+                                                  'end_date','rate_details_id')}
+    print("e",e)
+
+    get_code = { k : v for k,v in d.items() if k in ('ratecode_id','start_date','end_date')}
+    #print("get_code",get_code)
+
+    gensql('insert','revenue_management.rate_details',get_code)
+    rate_details_id = json.loads(gensql('select','revenue_management.rate_details','max(rate_details_id) as number1',get_code))
+    #print(rate_details_id)
+    e['rate_details_id'] = int(rate_details_id[0]['number1'])
     
+    room_id = json.loads(dbget("select max(rooms_id) as number1 from revenue_management.rooms_selected"))
+    #print("room_id",room_id[0]['number1'])    
     for i in d['room_types']:
         #pass
         dbput("insert into revenue_management.rooms_selected (rooms_id,room_type_id) \
-               values ('"+str(d['rooms_id'])+"','"+str(i)+"') ")
-    if d['rate_tier_id'] != 0:
-       dbput("delete from revenue_management.packages_selected where packages_id='"+str(d['packages_id'])+"' ")
-    
+               values ('"+str(room_id[0]['number1']+1)+"','"+str(i)+"') ")
+    e['rooms_id'] = int(room_id[0]['number1']+1)
+
+    #print("2222",e)
+    if d['rate_tier_id'] == 0:
+            
+       package_id = json.loads(dbget("select max(packages_id) as number2 from revenue_management.packages_selected"))
+       print("package_id",package_id[0]['number2'],type(package_id[0]['number2']))
        for j in d['package']:
-        #pass
-          dbput("insert into revenue_management.packages_selected (packages_id,package_code_id) \
-                values ('"+str(d['packages_id'])+"','"+str(j)+"')")
+         #print(j)
+         dbput("insert into revenue_management.packages_selected (packages_id,package_code_id) \
+               values ('"+str(package_id[0]['number2']+1)+"','"+str(j)+"')")
+       e['packages_id'] =  int(package_id[0]['number2']+1)
+    #print("3333",e)
 
-    b = { k : v for k,v in d.items() if k in ('rate_details_id')}
-    a = { k : v for k,v in d.items() if k not in ('rate_details_id','days','room_types','package')}
-    gensql('update','revenue_management.rate_details',a,b)
+    days = d['days']
+    day1 = [ k  for k,v in days.items() if v != 0 ]
+    #print(a,days,day1)
+    from_date = request.json['start_date']
+    to_date = request.json['end_date']
+    from_date = datetime.datetime.strptime(from_date,'%Y-%m-%d').date()
+    to_date = datetime.datetime.strptime(to_date,'%Y-%m-%d').date()
+
+    #print("4444",e)
     
-    return(json.dumps({'Status': 'Success', 'StatusCode': '200','Return': 'Record Updated Successfully','ReturnCode':'RUS'}, sort_keys=True, indent=4))
-
+    while from_date <= to_date:
+          print(from_date,from_date.strftime("%A")[0:3].lower())
+          if from_date.strftime("%A")[0:3].lower() in day1:
+              e['rate_date'] = from_date
+              gensql('insert','revenue_management.rates_all',e)
+          from_date+=datetime.timedelta(days=1)    
+    
+    return(json.dumps({"Return": "Record Updated Successfully",
+                       "ReturnCode": "RUS","Status": "Success","StatusCode": "200"},indent=4))
+ 
 
 
 
