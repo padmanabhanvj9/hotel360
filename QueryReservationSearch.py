6d944d960b4f0fa6f7ae9cadc51f216c0c92fb72
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-02-05 16:07:50 +0530

    Update QueryReservationSearch.py

--- QueryReservationSearch.py
+++ QueryReservationSearch.py
@@ -7,6 +7,6 @@ def QueryReservationSearch():
      #          full join profile.pf_company_profile on reservation.res_reservation.pf_id = profile.pf_company_profile.pf_id ")
     #sql_value = gensql('select','reservation.res_reservation','*')
     sql_value = json.loads(dbget("select pf_company_profile.pf_account, pf_company_profile.pf_type, res_reservation.* from reservation.res_reservation \
-	                      left join profile.pf_company_profile on pf_company_profile.pf_id = res_reservation.res_block"))
+	                      left join profile.pf_company_profile on pf_company_profile.pf_id = res_reservation.res_block where res_room_type not in ('PM')"))
     print(sql_value)
     return(json.dumps({'Status': 'Success', 'StatusCode': '200','ReturnValue':sql_value  ,'ReturnCode':'RRTS'},indent=4))
