d82721131c5f28a012032ff5d8227a8233bf1d4d
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-02-07 15:44:36 +0530

    inhouse record

--- HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
+++ HOTEL_CAH_GET_SELECT_QUERYINSHOUSERECORD.py
@@ -4,17 +4,17 @@ import json
 
 def HOTEL_CAH_POST_SELECT_QUERYINHOUSERECORD(request):
 
-    value1 = json.loads(dbget("SELECT pf_company_profile.pf_account,res_reservation.* FROM reservation.res_reservation \
+   value1 = json.loads(dbget("SELECT pf_company_profile.pf_account,res_reservation.* FROM reservation.res_reservation \
                                left join profile.pf_company_profile on pf_company_profile.pf_id = res_reservation.res_block \
                                where res_reservation.res_guest_status in ('checkin','due out') and res_room_type not in ('PM') "))
-    print(value1) 
-    value2 = json.loads(dbget("select pf_company_profile.pf_account,res_reservation.* from reservation.res_reservation \
+   #print(value1) 
+   value2 = json.loads(dbget("select pf_company_profile.pf_account,res_reservation.* from reservation.res_reservation \
                             left join profile.pf_company_profile on pf_company_profile.pf_id = res_reservation.res_block \
                              where CURRENT_DATE between res_arrival and res_depature and res_guest_status='Check out' and res_room_type not in ('PM') "))  
-    print(value2)
-    sql_value = value1+value2
-    print(sql_value)
-    for i in sql_value:
+   #print(value2)
+   sql_value = value1+value2
+   print(sql_value)
+   for i in sql_value:
       if i['res_block'] is None:
          if i['res_id'] in res_id_lists:
             pass
