4325015478602d3f3890d62a929bb570b9525605
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-02-22 15:52:04 +0530

    remove if condition

--- HOTEL_CAH_POST_INSERT_UPDATEPOSTINGPAYMENT.py
+++ HOTEL_CAH_POST_INSERT_UPDATEPOSTINGPAYMENT.py
@@ -55,19 +55,22 @@ def HOTEL_CAH_POST_INSERT_UPDATEPOSTINGPAYMENTCHECKOUT(request):
     s['Posting_description'] = "Payment posted successfully"
     gensql('insert','cashiering.posting_history_log',s)
     gensql('insert','cashiering.posting_original_history_log',s)
-    #dbput("update reservation.guest_deposit set total_amount = total_amount+'"+str(d['Postig_amount'])+"'\
-     #                   where res_id = '"+d['res_id']+"'")
-    balance = json.loads(dbget("select res_guest_balance from reservation.res_reservation \
-                                where res_room="+d.get("res_room")+" and res_id="+d.get("res_id")+" "))
-    print("balance",balance[0]['res_guest_balance'],type(balance[0]['res_guest_balance']))
-    if int(balance[0]['res_guest_balance']) != 0:
-        print(balance[0]['res_guest_balance'],type(balance[0]['res_guest_balance']))
-        res_id = request.json['res_id']
-        res_room = request.json['res_room']
-        sql_value = json.loads(dbget("select reservation.res_reservation.res_guest_status from reservation.res_reservation where res_id="+res_id+" and res_room="+res_room+" "))
-        balance1 = sql_value[0]['res_guest_status']
+    
+    #balance = json.loads(dbget("select res_guest_balance from reservation.res_reservation \
+    #                            where res_room="+d.get("res_room")+" and res_id="+d.get("res_id")+" "))
+    #print("balance: ", balance)
+    #print("bala",balance[0]['res_guest_balance'],type(balance[0]['res_guest_balance']))
+    #if int(balance[0]['res_guest_balance']) != 0:
+        #print(balance[0]['res_guest_balance'],type(balance[0]['res_guest_balance']))
+    res_id = request.json['res_id']
+    res_room = request.json['res_room']
+    sql_value = json.loads(dbget("select reservation.res_reservation.res_guest_status \
+                                  from reservation.res_reservation where res_id="+res_id+" and \
+                                  res_room="+res_room+" "))
+    print("sql", sql_value, type(sql_value))
+    balance1 = sql_value[0]['res_guest_status']
 
-        if balance1 =="due out":
+    if balance1 =="due out":
           status = "Check out"
           sql_value = dbput("update reservation.res_reservation set res_guest_status = '"+status+"' where res_id="+res_id+" and res_room="+res_room+" ")
           psql = dbput("update room_management.rm_room_list set rm_room_status = 'Dirty',rm_fo_status = 'vacant',rm_reservation_status = 'not reserved',rm_fo_person = '0' where rm_room ='"+str(res_room)+"'")
@@ -81,12 +84,9 @@ def HOTEL_CAH_POST_INSERT_UPDATEPOSTINGPAYMENTCHECKOUT(request):
           ac_log['RES_Description'] = "Reservation should be checkout.The room number is"+" "+str(res_room)
           ac_log['Res_id'] = str(res_id)
           sql_value = gensql('insert','reservation.res_activity_log',ac_log)
-        else:
-          return(json.dumps({'Status':'Failure','Return':'Unable to update'}, sort_keys=True, indent=4))
+    else:
+       return(json.dumps({'Status':'Failure','Return':'Unable to update'}, sort_keys=True, indent=4))
   
-        return(json.dumps({"Return": "Record Inserted Successfully", \
-                        "ReturnCode": "RIS","Status": "Success","StatusCode": "200", \
-                        "Balance":balance[0]['res_guest_balance']},indent=4))    
-    return(json.dumps({"Return": "Record Inserted Successfully","ReturnCode": "RIS","Status": "Success","StatusCode": "200","Balance":0},indent=4))
-  
+    return(json.dumps({"Return": "Record Inserted Successfully", \
+                        "ReturnCode": "RIS","Status": "Success","StatusCode": "200"},indent=4))     
 
