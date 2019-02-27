676d1f941f15bdb7ee34479b52e933b6fe9ea450
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-02-27 11:45:59 +0530

    reservation country changes

--- QueryReservationSearch.py
+++ QueryReservationSearch.py
@@ -6,7 +6,7 @@ def QueryReservationSearch():
 
 
     sql_value = json.loads(dbget("select pf_individual_profile.pf_firstname as firstname,pf_individual_profile.pf_lastname,\
-            pf_individual_profile.pf_language,pf_individual_profile.pf_title,pf_individual_profile.pf_mobileno,pf_individual_profile.pf_individual_vip,\
+            pf_individual_profile.pf_language,pf_individual_profile.pf_title,pf_individual_profile.pf_individual_country,pf_individual_profile.pf_mobileno,pf_individual_profile.pf_individual_vip,\
             pf_company_profile.pf_account, pf_company_profile.pf_type, res_reservation.* from reservation.res_reservation \
 	                      left join profile.pf_company_profile on pf_company_profile.pf_id = res_reservation.res_block \
 	                      left join profile.pf_individual_profile on pf_individual_profile.pf_id = res_reservation.pf_id where res_room_type not in ('PM')"))
