86b8f35beeaff9777b50e5fceea196895a379c06
iottech <iottech>
2019-02-28 17:48:57 +0530

    cashiering status changes

--- HOTEL_CASH_RESERVATION_STATUS.py
+++ HOTEL_CASH_RESERVATION_STATUS.py
@@ -2,14 +2,15 @@ from sqlwrapper import gensql, dbget, dbput
 import json
 import datetime
 
-
+from ApplicationDate import application_date
 
 def HOTEL_CASH_RESERVATION_STATUS(request):
     
     d = request.json
     ac_log = {}
-    RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
-    Posting_date = datetime.datetime.utcnow().date()
+    app_datetime = application_date()
+    RES_Log_Time = app_datetime[0]
+    Posting_date = app_datetime[1]
     res_id = request.json['res_id']
     res_room = request.json['res_room']
     sql_value = json.loads(dbget("select reservation.res_reservation.res_guest_status from reservation.res_reservation where res_id="+res_id+" and res_room="+res_room+" "))
@@ -23,9 +24,9 @@ def HOTEL_CASH_RESERVATION_STATUS(request):
                            rm_reservation_status = 'not reserved',rm_fo_person= '0',rm_room_status = 'Dirty' where rm_room = '"+str(res_room)+"' ")
         sql_value = dbput("update reservation.res_reservation set res_guest_status = '"+status+"' where res_id="+res_id+" and res_room="+res_room+" ")
         ac_log['Emp_Id'] = '121'
-        ac_log['Emp_Firstname'] = "Ranimangama"
+        ac_log['Emp_Firstname'] = "Admin"
         ac_log['RES_Log_Date'] = Posting_date
-        ac_log['RES_Log_Time'] = RES_Log_Time.time().strftime("%H:%M:%S")
+        ac_log['RES_Log_Time'] = app_datetime[2]
         ac_log['RES_Action_Type'] = "Checkout Reservation"
         ac_log['RES_Description'] = "Reservation should be checkout.The room number is"+" "+str(res_room)
         ac_log['Res_id'] = str(res_id)
@@ -41,8 +42,9 @@ def HOTEL_CASH_UpdateReinstateRervaiton(request):
     
     d = request.json
     ac_log = {}
-    RES_Log_Time = datetime.datetime.utcnow()+datetime.timedelta(hours=5, minutes=30)
-    Posting_date = datetime.datetime.utcnow().date()
+    app_datetime = application_date()
+    RES_Log_Time = app_datetime[0]
+    Posting_date = app_datetime[1]
     res_id = request.json['res_id']
     res_room = request.json['res_room']
     sql_value = json.loads(dbget("select * from reservation.res_reservation \
@@ -60,9 +62,9 @@ def HOTEL_CASH_UpdateReinstateRervaiton(request):
         sql_value = dbput("update reservation.res_reservation set res_guest_status = '"+status+"' \
                            where res_id="+res_id+" and res_room="+res_room+" ")
         ac_log['Emp_Id'] = '121'
-        ac_log['Emp_Firstname'] = "Ranimangama"
+        ac_log['Emp_Firstname'] = "Admin"
         ac_log['RES_Log_Date'] = Posting_date
-        ac_log['RES_Log_Time'] = RES_Log_Time.time().strftime("%H:%M:%S")
+        ac_log['RES_Log_Time'] = app_datetime[2]
         ac_log['RES_Action_Type'] = "Due out Reservation"
         ac_log['RES_Description'] = "Reservation should be due out.The room number is"+" "+str(res_room)
         ac_log['Res_id'] = str(res_id)
