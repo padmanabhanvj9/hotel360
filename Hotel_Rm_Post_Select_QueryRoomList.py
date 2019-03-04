940f7969d508698be0e77c0d7b66d595ae8e752c
Padmanabhan K <padmanabhanvj9@gmail.com>
2019-03-04 13:02:37 +0530

    queryroomlist

--- Hotel_Rm_Post_Select_QueryRoomList.py
+++ Hotel_Rm_Post_Select_QueryRoomList.py
@@ -1,4 +1,4 @@
-from sqlwrapper import dbget
+from sqlwrapper import dbget,gensql,dbput
 import json
 
 def hotel_rm_post_select_queryroomlist(request):
@@ -9,6 +9,21 @@ def hotel_rm_post_select_queryroomlist(request):
 
 def hotel_rm_post_insert_roomcount(request):
    d = request.json
-   gensql('insert','room_management.room_available',d)
-   return(json.dumps({"Return": "Record Inserted Successfully","ReturnCode": "RIS",
+
+   sqlcount = json.loads(dbget("select count(*) from room_management.room_available where rm_room = '"+str(d['rm_room'])+"' \
+                                and rm_date = '"+str(d['rm_date'])+"'"))
+   print(sqlcount)
+   if sqlcount[0]['count'] == 0:
+       gensql('insert','room_management.room_available',d)
+       return(json.dumps({"Return": "Record Inserted Successfully","ReturnCode": "RIS",
                       "Status": "Success","StatusCode": "200"},indent=4))
+
+   else:
+       print("else part")
+
+       dbput("update room_management.room_available set available_count = available_count + "+str(d['available_count'])+" ,total_count = total_count + "+str(d['total_count'])+" \
+            where rm_room = '"+str(d['rm_room'])+"' and rm_date= '"+str(d['rm_date'])+"' ")
+       return(json.dumps({"Return": "Record Updated Successfully","ReturnCode": "RUS",
+                      "Status": "Success","StatusCode": "200"},indent=4))
+
+   
